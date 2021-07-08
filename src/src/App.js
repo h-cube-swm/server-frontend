82baff54d941cbfbce966ecd455dc7b34e1aa2b7
nyeok98 <dn.shin98@gmail.com>
2021-07-08 10:32:48 +0000

    Initial commit

---
+++ src/src/App.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+import './App.css'
+import MakeSurvey from './components/MakeSurvey';
+
+function App() {
+  return (
+    <MakeSurvey />
+  );
+}
+
+export default App;
---
+++ src/src/components/MakeSurvey.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../style.css';
+
+function MakeSurvey() {
+    return (
+        <a href="#" className="btn-make-survey">
+            <div>
+                설문조사 하러가기
+            </div>
+        </a>
+    );
+}
+
+export default MakeSurvey;
