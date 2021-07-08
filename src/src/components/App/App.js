2ec452807d8fe1fe39787681f17dc111c7976fd2
nyeok98 <dn.shin98@gmail.com>
2021-07-08 19:08:22 +0000

    Add main page

---
+++ src/src/components/App/App.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+import './App.css'
+import Intro from '../Intro/Intro';
+import Header from '../Header/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Intro />
+    </div>
+  );
+}
+
+export default App;
