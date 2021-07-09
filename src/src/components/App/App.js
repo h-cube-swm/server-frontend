b165d270402f1fb51e8ec29ef342d4731202e80e
nyeok98 <dn.shin98@gmail.com>
2021-07-09 13:49:59 +0000

    Add service component

--- src/src/components/App/App.js
+++ src/src/components/App/App.js
@@ -9,7 +9,7 @@ function App() {
     <div>
       <Header />
       <Intro />
-      {/* <Service /> */}
+      <Service />
     </div>
   );
 }
--- src/src/components/Intro/Intro.js
+++ src/src/components/Intro/Intro.js
@@ -17,9 +17,7 @@ function Intro() {
                             <strong>더 폼</strong>
                         </h1>
                         <a href="#" className="btn-make-survey">
-                            <div>
-                                설문조사 하러가기
-                            </div>
+                            설문조사 하러가기
                         </a>
                     </div>
                 </div>
