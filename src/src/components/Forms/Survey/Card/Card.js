a1e9450f17032010029ccf99303c7d7991fd6cac
unknownpgr <unknownpgr@gmail.com>
2021-07-19 12:10:46 +0000

    Implement card-question system

--- src/src/components/Forms/Survey/Card/Card.js
+++ src/src/components/Forms/Survey/Card/Card.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import { CardTypes, CardStates } from '../constants';
+import '../Card/Card.scss';
 
-export default function Card({ question, mode, setQuestion, setResponse, index, selectedIndex }) {
+export default function Card({ question, state, setQuestion, setResponse, index, selectedIndex }) {
+
+    const { type } = question;
+    const style = {
+        transform: 'translate(-50%,-50%)'
+    };
+
+    switch (type) {
+
+    }
+
+    switch (state) {
+
+    }
+
     return (
-        <div>
-            {this.CARD_MODE_RESPONSE}
+        <div className="survey-card" style={style}>
+            {CardTypes.CARD_MODE_RESPONSE}
             <div>Question : {JSON.stringify(question)}</div>
-            <div>Mode : {mode}</div>
+            <div>Type : {type}</div>
+            <div>State: {state}</div>
             <div>Index : {index}</div>
             <div>SelectedIndex : {selectedIndex}</div>
         </div>
     );
-}
 };
--- src/src/components/Forms/Survey/Edit.js
+++ src/src/components/Forms/Survey/Edit.js
@@ -2,30 +2,64 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getApi } from "../../../utils/parser";
+import { CardTypes, CardStates } from "./constants";
+import Card from "./Card/Card";
+import './Edit.scss';
 
 /* Components */
 import Controller from "./Controller/Controller";
 
 const Edit = ({ match }) => {
-  const [survey, setSurvey] = useState(null);
+  const [survey, setSurvey] = useState({ questions: [] });
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
-  const [selectedElement, setSelectedElement] = useState("");
   const surveyId = match.params.link;
 
   useEffect(async () => {
-    const survey = await getApi(`/surveys/${surveyId}`);
+    const { result: survey } = await getApi(`/surveys/${surveyId}`);
+    if (!survey.counter) survey.counter = 0;
+    if (!survey.questions) survey.questions = [];
+    if (survey.questions.length == 0) survey.questions.push({
+      id: survey.counter++,
+      type: CardTypes.SINGLE_CHOICE
+    });
     setSurvey(survey);
   }, []);
 
-  console.log(survey);
+  const setSelectedSurveyType = (type) => {
+    setSurvey(survey => {
+      const questions = [...survey.questions];
+      questions[selectedIndex].type = type;
+      return {
+        questions,
+        ...survey
+      };
+    });
+  };
+
+  const selectedSurveyType = survey.questions[selectedIndex]?.type;
+  const sortedQuestions = [...survey?.questions].sort((a, b) => a.id - b.id);
+  const idList = survey.questions.map(({ id }) => id);
 
   return (
-    <div>
+    <div className="edit">
       <div style={{ opacity: survey ? 0 : 1 }}>Loading</div>
       <h1>Edit Page</h1>
-      <p>{surveyId}</p>
-      <Controller element={selectedElement} setElement={setSelectedElement} />
-      <h2>{selectedElement}</h2>
+      <Controller element={selectedSurveyType} setElement={setSelectedSurveyType} />
+      <div className="question-container">
+        {sortedQuestions.map((question => {
+          const index = idList.indexOf(question.id);
+          return (
+            <Card
+              question={question}
+              index={index}
+              selectedIndex={selectedIndex}
+              mode={CardStates.BLURREED}
+            >
+            </Card>
+          );
+        }))}
+      </div>
       <Link to={"/forms/survey/end/" + match.params.link}>완료</Link>
     </div>
   );
