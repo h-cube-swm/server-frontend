6b63d9fe018fe2f26eebb2192c70ed29c77902a5
unknownpgr <unknownpgr@gmail.com>
2021-07-19 12:11:30 +0000

    Refactor controller code to be DRY

--- src/src/components/Forms/Survey/Controller/Controller.js
+++ src/src/components/Forms/Survey/Controller/Controller.js
@@ -1,18 +1,36 @@
 import React, { useState } from "react";
+import "./Controller.scss";
+import singleChoice from "../../../../assets/icons/single-choice.svg";
+import multipleChoice from "../../../../assets/icons/multiple-choice.svg";
+import preference from "../../../../assets/icons/preference.svg";
+import shortSentence from "../../../../assets/icons/short-sentence.svg";
+import longSentence from "../../../../assets/icons/long-sentence.svg";
+import { CardTypes } from "../constants";
 
-export default function Controller({ setElement }) {
-  const onClick = (e) => {
-    setElement(e.target.innerText);
-  };
+const buttons = [
+  [singleChoice, CardTypes.SINGLE_CHOICE],
+  [multipleChoice, CardTypes.MULTIPLE_CHOICE],
+  [preference, CardTypes.PREFERENCE],
+  [shortSentence, CardTypes.SHORT_SENTENCE],
+  [longSentence, CardTypes.LONG_SENTENCE],
+];
 
+export default function Controller({ element, setElement }) {
   return (
     <div>
       <div className="element-box">
-        <button onClick={onClick}>단일선택</button>
-        <button onClick={onClick}>다중선택</button>
-        <button onClick={onClick}>선호도</button>
-        <button onClick={onClick}>단답형</button>
-        <button onClick={onClick}>장문형</button>
+        {buttons.map(([image, type]) => {
+          const className = type === element ? "selected" : "";
+          return (
+            <button
+              key={type}
+              className={"element-btn " + className}
+              onClick={() => setElement(type)}
+            >
+              <img src={image} alt={type} />
+            </button>
+          );
+        })}
       </div>
     </div>
   );
