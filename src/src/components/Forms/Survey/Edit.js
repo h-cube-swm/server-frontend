a1fd3d4e12bce69cf0c0e4a8fc856c5e3b0b8cfc
unknownpgr <unknownpgr@gmail.com>
2021-07-19 07:45:54 +0000

    Implement survey data load

--- src/src/components/Forms/Survey/Edit.js
+++ src/src/components/Forms/Survey/Edit.js
@@ -1,5 +1,5 @@
 /* React elements*/
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getApi } from "../../../utils/parser";
 
@@ -7,17 +7,27 @@ import { getApi } from "../../../utils/parser";
 import Controller from "./Controller/Controller";
 
 const Edit = ({ match }) => {
+  const [survey, setSurvey] = useState(null);
+
   const [selectedElement, setSelectedElement] = useState("");
-  const survey_id = match.params.link;
+  const surveyId = match.params.link;
+
+  useEffect(async () => {
+    const survey = await getApi(`/surveys/${surveyId}`);
+    setSurvey(survey);
+  }, []);
+
+  console.log(survey);
 
   return (
-    <>
+    <div>
+      <div style={{ opacity: survey ? 0 : 1 }}>Loading</div>
       <h1>Edit Page</h1>
-      <p>{survey_id}</p>
-      <Controller setElement={setSelectedElement} />
+      <p>{surveyId}</p>
+      <Controller element={selectedElement} setElement={setSelectedElement} />
       <h2>{selectedElement}</h2>
       <Link to={"/forms/survey/end/" + match.params.link}>완료</Link>
-    </>
+    </div>
   );
 };
 
