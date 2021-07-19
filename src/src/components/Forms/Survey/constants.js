bcbd1ff41697e87d198c07a8329271819bb2e745
unknownpgr <unknownpgr@gmail.com>
2021-07-19 11:25:45 +0000

    Add constants for cards

---
+++ src/src/components/Forms/Survey/constants.js
@@ -0,0 +1,12 @@
+export const CardModes = {
+    SINGLE_CHOICE: 'single-choicd',
+    MULTIPLE_CHOICE: 'multiple-choice',
+    PREFERENCE: 'preference',
+    SHORT_SENTENCE: 'short-sentence',
+    LONG_SENTENCE: 'long-sentence'
+};
+
+export const CardStates = {
+    CARD_MODE_EDITTING: 0x01,
+    CARD_MODE_RESPONSE: 0x02,
+};
