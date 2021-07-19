ea9b33dddf3f93dedec2cce3132289f946f570cf
SeungWookHan <hswook12@me.com>
2021-07-19 13:32:31 +0000

    Add form tag with input tags - #12

--- src/src/components/Forms/Survey/Prologue/Prologue.js
+++ src/src/components/Forms/Survey/Prologue/Prologue.js
@@ -1,5 +1,18 @@
 import React, { useState, useEffect } from "react";
 export default function Prologue() {
   return (
+        <form>
+          <input
+            name="title"
+            type="text"
+            placeholder="설문 제목을 입력해주세요"
+          />
+          <br />
+          <input
+            name="description"
+            type="text"
+            placeholder="설문 설명을 입력해주세요"
+          />
+        </form>
   );
 }
