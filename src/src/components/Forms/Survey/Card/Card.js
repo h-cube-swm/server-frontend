54812ad1f69bf15b1ef4badf14a7e29abd4d8a71
unknownpgr <unknownpgr@gmail.com>
2021-07-19 15:45:34 +0000

    Add appear delay

--- src/src/components/Forms/Survey/Card/Card.js
+++ src/src/components/Forms/Survey/Card/Card.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { CardTypes, CardStates } from '../constants';
 import '../Card/Card.scss';
 
 export default function Card({ question, state, setQuestion, setResponse, index, selectedIndex }) {
 
+    const [isInit, setIsInit] = useState(true);
     const { type } = question;
     const deltaIndex = index - selectedIndex;
     const isSelected = deltaIndex === 0;
     const yPos = deltaIndex * 540;
 
+    useEffect(() => {
+        setTimeout(() => {
+            setIsInit(false);
+        }, 400);
+    }, []);
+
     const style = {
-        transform: 'translate(-50%, -50%)'
+        transform: 'translate(-50%, -50%)',
+        opacity: isInit ? 0 : 1
     };
 
     switch (type) {
