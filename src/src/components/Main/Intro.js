dd28fb217ab18c887810bed1e3174fba575836e6
nyeok98 <dn.shin98@gmail.com>
2021-07-20 12:53:32 +0000

    Add mixin to refactor scss

--- src/src/components/Main/Intro.js
+++ src/src/components/Main/Intro.js
@@ -9,29 +9,27 @@ import character2 from "../../assets/images/main-character-2.svg";
 
 function Intro() {
   return (
-    <>
-      <div className="row-1">
-        <div className="character1">
-          <img src={character1} alt="counting man" />
-        </div>
-        <div className="outer-box">
-          <div className="intro">
-            <h1>
-              모든 폼을 대신하는
-              <br />더 폼나는 설문조사
-              <br />
-              <strong>더 폼</strong>
-            </h1>
-            <Link className="btn-make-survey" to="/forms/survey">
-              설문조사 하러가기
-            </Link>
-          </div>
-        </div>
-        <div className="character2">
-          <img src={character2} alt="counting man" />
+    <div className="row-1">
+      <div className="character1">
+        <img src={character1} alt="counting man" />
+      </div>
+      <div className="outer-box">
+        <div className="intro">
+          <h1>
+            모든 폼을 대신하는
+            <br />더 폼나는 설문조사
+            <br />
+            <strong>더 폼</strong>
+          </h1>
+          <Link className="btn-make-survey" to="/forms/survey">
+            설문조사 하러가기
+          </Link>
         </div>
       </div>
-    </>
+      <div className="character2">
+        <img src={character2} alt="counting man" />
+      </div>
+    </div>
   );
 }
 
