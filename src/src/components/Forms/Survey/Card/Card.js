7b0ae6a36003d0d885c3d5b0a171929b4db30a2a
unknownpgr <unknownpgr@gmail.com>
2021-07-20 05:07:47 +0000

    Update card design and logic

--- src/src/components/Forms/Survey/Card/Card.js
+++ src/src/components/Forms/Survey/Card/Card.js
@@ -4,83 +4,86 @@ import "../Card/Card.scss";
 import hanleImage from "../../../../assets/icons/handle.svg";
 
 export default function Card({
-  question,
-  state,
-  setQuestion,
-  setResponse,
-  index,
-  selectedIndex,
-  onGrab,
-  dom,
+	// Logic-associated parameters
+	question,
+	state,
+	setQuestion,
+	setResponse,
+
+	// UI-associated parameters
+	index,
+	selectedIndex,
+	onGrab,
+	dom,
 }) {
-  const [isInit, setIsInit] = useState(true);
-  //ToDo : 여기서 에러 발생함. question 이 null인 경우 있음!
-  const { type } = question;
-  const deltaIndex = index - selectedIndex;
-  const yPos = deltaIndex * (CardStyle.HEIGHT + CardStyle.DISTANCE);
-  let showHandle = false;
+	const [isInit, setIsInit] = useState(true);
+	const yPos =
+		(index - selectedIndex) * (CardStyle.HEIGHT + CardStyle.DISTANCE);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setIsInit(false);
-    }, 400);
-  }, []);
+	// Implement delayed fade-in effect
+	useEffect(() => {
+		setTimeout(() => {
+			setIsInit(false);
+		}, 400);
+	}, []);
 
-  let style = {
-    transform: "translate(-50%, -50%)",
-    opacity: isInit ? 0 : 1,
-    height: CardStyle.HEIGHT,
-  };
+	let classes = ["survey-card"];
 
-  switch (type) {
-  }
+	let style = {
+		height: CardStyle.HEIGHT,
+	};
+	if (isInit) style.opacity = 0;
 
-  switch (state) {
-    case CardStates.EDITTING:
-      style.transform += ` translateY(${yPos}px)`;
-      showHandle = true;
-      break;
-    case CardStates.ORDERING:
-      style.opacity = 0;
-      style.transitionDuration = 0 + "s";
-      break;
-    case CardStates.RESPONSE:
-      style.transform += ` translateY(${yPos}px)`;
-      break;
-    case CardStates.GHOST:
-      showHandle = true;
-      style.transform = "translate(-660px,-50%)";
-      style.transitionDuration = 0 + "s";
-      if (index == 0) {
-        style.opacity = 0;
-        style.zIndex = -1;
-      }
-      break;
-  }
+	switch (state) {
+		case CardStates.EDITTING:
+			style.transform = `translate(-50%, -50%) translateY(${yPos}px)`;
+			classes.push("highlight");
+			classes.push("show-handle");
+			break;
 
-  const _onGrab = (event) => {
-    if (!showHandle) return;
-    event.preventDefault();
-    onGrab();
-  };
+		case CardStates.ORDERING:
+			style.transform = `translate(-50%, -50%) translateY(${yPos}px)`;
+			classes.push("hidden");
+			break;
 
-  return (
-    <div className="survey-card" style={style} ref={dom}>
-      {CardTypes.CARD_MODE_RESPONSE}
-      <div>Question : {JSON.stringify(question)}</div>
-      <div>Type : {type}</div>
-      <div>State: {state}</div>
-      <div>Index : {index}</div>
-      <div>SelectedIndex : {selectedIndex}</div>
-      <div
-        className="handle"
-        onMouseDown={_onGrab}
-        style={{
-          opacity: showHandle ? 1 : 0,
-          cursor: showHandle ? "grab" : "inherit",
-        }}>
-        <img src={hanleImage}></img>
-      </div>
-    </div>
-  );
+		case CardStates.RESPONSE:
+			style.transform = `translate(-50%, -50%) translateY(${yPos}px)`;
+			break;
+
+		case CardStates.GHOST:
+			if (index == 0) {
+				classes.push("hidden");
+			} else {
+				classes.push("ghost");
+				classes.push("show-handle");
+				classes.push("highlight");
+			}
+			break;
+	}
+
+	const { type } = question;
+	switch (type) {
+	}
+
+	const _onGrab = (event) => {
+		event.preventDefault();
+		if (state !== CardStates.EDITTING) return;
+		onGrab();
+	};
+
+	const className = classes.join(" ");
+
+	return (
+		<div className={className} style={style} ref={dom}>
+			{CardTypes.CARD_MODE_RESPONSE}
+			<div>Question : {JSON.stringify(question)}</div>
+			<div>Type : {type}</div>
+			<div>State: {state}</div>
+			<div>Index : {index}</div>
+			<div>SelectedIndex : {selectedIndex}</div>
+			<div className="handle" onMouseDown={_onGrab}>
+				<img src={hanleImage}></img>
+			</div>
+		</div>
+	);
 }
