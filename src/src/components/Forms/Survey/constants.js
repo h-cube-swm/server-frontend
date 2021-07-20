580a995d88302ddbcef748a8e1cc0f69dd11cd4c
unknownpgr <unknownpgr@gmail.com>
2021-07-20 04:30:34 +0000

    Refactor code style

--- src/src/components/Forms/Survey/constants.js
+++ src/src/components/Forms/Survey/constants.js
@@ -1,19 +1,19 @@
 export const CardTypes = {
-    SINGLE_CHOICE: 'single-choice',
-    MULTIPLE_CHOICE: 'multiple-choice',
-    PREFERENCE: 'preference',
-    SHORT_SENTENCE: 'short-sentence',
-    LONG_SENTENCE: 'long-sentence'
+  SINGLE_CHOICE: 'single-choice',
+  MULTIPLE_CHOICE: 'multiple-choice',
+  PREFERENCE: 'preference',
+  SHORT_SENTENCE: 'short-sentence',
+  LONG_SENTENCE: 'long-sentence'
 };
 
 export const CardStates = {
-    RESPONSE: 'response',
-    EDITTING: 'editting',
-    ORDERING: 'ordering',
-    GHOST: 'ghost'
+  RESPONSE: 'response',
+  EDITTING: 'editting',
+  ORDERING: 'ordering',
+  GHOST: 'ghost'
 };
 
 export const CardStyle = {
-    HEIGHT: 250,
-    DISTANCE: 100
+  HEIGHT: 250,
+  DISTANCE: 100
 };
