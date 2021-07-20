9cfbea5f9ce1cb5be3faab4b2ae4c1713ccd8b3b
nyeok98 <dn.shin98@gmail.com>
2021-07-20 19:10:12 +0000

    Adjust style component

--- src/src/components/Forms/Survey/Ending/Ending.js
+++ src/src/components/Forms/Survey/Ending/Ending.js
@@ -5,20 +5,20 @@ import { putApi } from "../../../../utils/parser";
 import "./Ending.scss";
 
 const Ending = ({ match }) => {
-  const [title, setTitle] = useState("제목을 입력해주세요.");
-  const [description, setDescription] = useState("제목을 입력해주세요.");
-  const [surveyLink, setSurveyLink] = useState("test");
-  const [resultLink, setResultLink] = useState("test");
+  const [title, setTitle] = useState("Loading...");
+  const [description, setDescription] = useState("Loading...");
+  const [surveyLink, setSurveyLink] = useState("Loading...");
+  const [resultLink, setResultLink] = useState("Loading...");
   const survey_id = match.params.link;
 
   useEffect(async () => {
     try {
       const json = await putApi(`/surveys/${survey_id}/end`);
       const obj = JSON.parse(JSON.stringify(json));
-      setTitle(obj.result[0].fields.title);
-      setDescription(obj.result[0].fields.description);
-      setSurveyLink(obj.result[0].fields.survey_link);
-      setResultLink(obj.result[0].fields.result_link);
+      setTitle(obj.result.title);
+      setDescription(obj.result.description);
+      setSurveyLink(obj.result.survey_link);
+      setResultLink(obj.result.result_link);
     } catch (e) {
       console.log(e);
     }
@@ -26,13 +26,26 @@ const Ending = ({ match }) => {
 
   return (
     <div className="ending-page">
+      <h1 className="celebrate-sentence">
+        축하합니다. 설문을 완성했습니다.
+        <br />
+        이제 배포만 남았습니다.
+      </h1>
       <div className="survey-sum">
-        <h1>제목: {title}</h1>
-        <h2>설명: {description}</h2>
-        <p>배포링크: {surveyLink}</p>
-        <p>결과링크: {resultLink}</p>
+        <h1>{title}</h1>
+        <h2>{description}</h2>
+        <ul>
+          <li>
+            <h3>배포링크</h3>
+            <p>{"https://the-form.io/forms/survey/response/" + surveyLink}</p>
+          </li>
+          <li>
+            <h3>결과링크</h3>
+            <p>{"https://the-form.io/forms/survey/result/" + resultLink}</p>
+          </li>
+        </ul>
       </div>
-      <Link className="home-btn" to="/">
+      <Link className="link-btn-lg" to="/">
         홈으로
       </Link>
     </div>
