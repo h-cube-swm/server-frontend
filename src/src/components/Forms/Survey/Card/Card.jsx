d9379bc320cb931e06f7403a5964d19fba6c4ba7
nyeok98 <dn.shin98@gmail.com>
2021-07-20 19:11:04 +0000

    Set evironment for making toggle switch

--- src/src/components/Forms/Survey/Card/Card.jsx
+++ src/src/components/Forms/Survey/Card/Card.jsx
@@ -2,111 +2,125 @@ import React, { useEffect, useState } from "react";
 import { CardTypes, CardStates, CardStyle } from "../constants";
 import "../Card/Card.scss";
 import hanleImage from "../../../../assets/icons/handle.svg";
+import ToggleSwitch from "./ToggleSwitch";
 
 export default function Card({
-	// Logic-associated parameters
-	question,
-	state,
-	setQuestion,
-	setResponse,
+  // Logic-associated parameters
+  question,
+  state,
+  setQuestion,
+  setResponse,
 
-	onDelete,
+  onDelete,
 
-	// UI-associated parameters
-	index,
-	selectedIndex,
-	onGrab,
-	dom,
+  // UI-associated parameters
+  index,
+  selectedIndex,
+  onGrab,
+  dom,
   slowAppear,
 }) {
-	const [isInit, setIsInit] = useState(true);
-	const yPos =
-		(index - selectedIndex) * (CardStyle.HEIGHT + CardStyle.DISTANCE);
+  const [isInit, setIsInit] = useState(true);
+  const yPos =
+    (index - selectedIndex) * (CardStyle.HEIGHT + CardStyle.DISTANCE);
 
-	// Implement delayed fade-in effect
-	useEffect(() => {
+  // Implement delayed fade-in effect
+  useEffect(() => {
     if (slowAppear) {
-		const handle = setTimeout(() => {
-			setIsInit(false);
-		}, 400);
-		return () => clearTimeout(handle);
+      const handle = setTimeout(() => {
+        setIsInit(false);
+      }, 400);
+      return () => clearTimeout(handle);
     } else {
       setIsInit(false);
     }
-	}, []);
+  }, []);
 
-	let classes = ["survey-card"];
+  if (!question) return null;
 
-	let style = {
-		height: CardStyle.HEIGHT,
-	};
-	if (isInit) style.opacity = 0;
+  let classes = ["survey-card"];
 
-	switch (state) {
-		case CardStates.EDITTING:
-			style.transform = `translate(-50%, -50%) translateY(${yPos}px)`;
-			classes.push("highlight");
-			classes.push("show-handle");
-			break;
+  let style = {
+    height: CardStyle.HEIGHT,
+  };
+  if (isInit) style.opacity = 0;
 
-		case CardStates.ORDERING:
-			style.transform = `translate(-50%, -50%) translateY(${yPos}px)`;
-			classes.push("hidden");
-			break;
+  switch (state) {
+    case CardStates.EDITTING:
+      style.transform = `translate(-50%, -50%) translateY(${yPos}px)`;
+      classes.push("highlight");
+      classes.push("show-handle");
+      break;
 
-		case CardStates.RESPONSE:
-			style.transform = `translate(-50%, -50%) translateY(${yPos}px) scale(0.975)`;
-			break;
+    case CardStates.ORDERING:
+      style.transform = `translate(-50%, -50%) translateY(${yPos}px)`;
+      classes.push("hidden");
+      break;
 
-		case CardStates.GHOST:
-			if (index == 0) {
-				classes.push("hidden");
-			} else {
-				classes.push("ghost");
-				classes.push("show-handle");
-				classes.push("highlight");
-			}
-			break;
-	}
+    case CardStates.RESPONSE:
+      style.transform = `translate(-50%, -50%) translateY(${yPos}px) scale(0.975)`;
+      break;
 
-	let inner = <div className="loading">SAMPLE : {question?.id}</div>;
+    case CardStates.GHOST:
+      if (index == 0) {
+        classes.push("hidden");
+      } else {
+        classes.push("ghost");
+        classes.push("show-handle");
+        classes.push("highlight");
+      }
+      break;
+  }
 
-	const type = question?.type;
-	switch (type) {
-		case CardTypes.SINGLE_CHOICE:
-			break;
-		case CardTypes.MULTIPLE_CHOICE:
-			break;
-		case CardTypes.PREFERENCE:
-			break;
-		case CardTypes.SHORT_SENTENCE:
-			break;
-		case CardTypes.LONG_SENTENCE:
-			break;
-		default:
-			break;
-	}
+  let inner;
 
-	const _onGrab = (event) => {
-		event.preventDefault();
-		if (state !== CardStates.EDITTING) return;
-		onGrab();
-	};
+  const type = question?.type;
+  switch (type) {
+    case CardTypes.SINGLE_CHOICE:
+      break;
+    case CardTypes.MULTIPLE_CHOICE:
+      break;
+    case CardTypes.PREFERENCE:
+      break;
+    case CardTypes.SHORT_SENTENCE:
+      break;
+    case CardTypes.LONG_SENTENCE:
+      break;
+    default:
+      inner = <div className="loading">SAMPLE : {question?.id}</div>;
+      break;
+  }
 
-	const className = classes.join(" ");
+  const _onGrab = (event) => {
+    event.preventDefault();
+    if (state !== CardStates.EDITTING) return;
+    onGrab();
+  };
 
-	return (
-		<div className={className} style={style} ref={dom}>
-			<div className="inner-container">{inner}</div>
-			<div className="handle" onMouseDown={_onGrab}>
-				<img src={hanleImage}></img>
-			</div>
-			<button
-				className="delete"
-				onClick={onDelete}
-				hidden={state !== CardStates.EDITTING}>
-				DELETE
-			</button>
-		</div>
-	);
+  const className = classes.join(" ");
+  console.log(question);
+  return (
+    <div className={className} style={style} ref={dom}>
+      <div className="card-header">
+        <input type="text" placeholder="제목을 입력하세요." />
+        <ToggleSwitch
+          isRequired={question.isRequired}
+          setIsRequired={(isRequired) => {
+            const newQuestion = { ...question, isRequired };
+            setQuestion(newQuestion);
+          }}
+        />
+      </div>
+      <div className="inner-container">{inner}</div>
+      <div className="handle" onMouseDown={_onGrab}>
+        <img src={hanleImage}></img>
+      </div>
+      <button
+        className="delete"
+        onClick={onDelete}
+        hidden={state !== CardStates.EDITTING}>
+        삭제
+      </button>
+    </div>
+  );
 }
---
+++ src/src/components/Forms/Survey/Card/ToggleSwitch.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import "./ToggleSwitch.scss";
+import selected_dot from "../../../../assets/icons/selected-dot.svg";
+import unselected_dot from "../../../../assets/icons/unselected-dot.svg";
+
+export default function ToggleSwitch() {
+  return <div></div>;
+}
