3dfe318945b1b9acb958b1664d8e3f1188164683
unknownpgr <unknownpgr@gmail.com>
2021-07-20 11:51:47 +0000

    Fix main page scroll lock error

--- src/src/components/Main/Main.js
+++ src/src/components/Main/Main.js
@@ -4,13 +4,15 @@ import Intro from './Intro';
 import Header from './Header';
 import Service from './Service';
 
+import './Main.scss';
+
 const Main = () => {
     return (
-        <>
+        <div className="main-page">
             <Header />
             <Intro />
             <Service />
-        </>
+        </div>
     );
 };
 
