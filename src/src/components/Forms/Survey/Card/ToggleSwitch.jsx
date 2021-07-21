33992bf2517580f3c2004f33fde85e37c2076cfa
nyeok98 <dn.shin98@gmail.com>
2021-07-21 13:03:31 +0000

    Add toggle switch component

--- src/src/components/Forms/Survey/Card/ToggleSwitch.jsx
+++ src/src/components/Forms/Survey/Card/ToggleSwitch.jsx
@@ -1,8 +1,25 @@
 import React, { useState } from "react";
 import "./ToggleSwitch.scss";
-import selected_dot from "../../../../assets/icons/selected-dot.svg";
-import unselected_dot from "../../../../assets/icons/unselected-dot.svg";
+import selectedDot from "../../../../assets/icons/selected-dot.svg";
+import unselectedDot from "../../../../assets/icons/unselected-dot.svg";
 
-export default function ToggleSwitch() {
-  return <div></div>;
+export default function ToggleSwitch(params) {
+  const isRequired = params.isRequired;
+  const onClick = () => {
+    params.setIsRequired(!params.isRequired);
+  };
+
+  return (
+    <div className="toggle-switch">
+      <div className="toggle-box" />
+      <button
+        className={isRequired ? "toggle-btn" : "unselected-toggle-btn"}
+        onClick={onClick}>
+        <img
+          src={isRequired ? selectedDot : unselectedDot}
+          alt="require element switch"
+        />
+      </button>
+    </div>
+  );
 }
