3747ec22a941d9aa33ed8b367bbb33634447b48f
nyeok98 <dn.shin98@gmail.com>
2021-07-22 15:54:57 +0000

    Add max length and format limit to short sentece component

--- src/src/components/Forms/Survey/Card/Types/ShortSentence/ShortSentence.jsx
+++ src/src/components/Forms/Survey/Card/Types/ShortSentence/ShortSentence.jsx
@@ -5,6 +5,7 @@ import "./ShortSentence.scss";
 import plusBtn from "../../../../../../assets/icons/add-btn.svg";
 import minusBtn from "../../../../../../assets/icons/minus-btn.svg";
 import TextField from "../../../../../TextField/TextField";
+import Hider from "../../../../../Hider/Hider";
 
 export default function ShortSentence({
   question,
@@ -14,17 +15,28 @@ export default function ShortSentence({
 }) {
   const [curLen, setCurLen] = useState(0);
   const initialized = useDefault(question, setQuestion, {
-      answer: "",
-      maxLen: 32,
+    answer: "",
+    maxLen: 32,
   });
   if (!initialized) return null;
 
   const update = (e) => {
     const answer = e.target.value;
+    if (e.target.value.length <= question.maxLen) {
+      setCurLen(e.target.value.length);
+    }
     setQuestion((question) => ({ ...question, answer }));
   };
 
+  const maxLenChange = (e) => {
+    let text = e.target.value + "";
+    if (!/^[0-9]{0,3}$/.test(text)) return false;
+    const maxLen = +text;
+    setQuestion((question) => ({ ...question, maxLen }));
+  };
+
   const plus = () => {
+    if (question.maxLen >= 999) return;
     const maxLen = question.maxLen + 1;
     setQuestion((question) => ({ ...question, maxLen }));
   };
@@ -39,32 +51,48 @@ export default function ShortSentence({
     return (
       <div className="short-sentence">
         <TextField placeholder="단답형 텍스트" disabled />
-        <div className="max-len-controller">
-          <div className="text-set">
-            <p>최대 글자수</p>
-            <strong>{question.maxLen}</strong>
+        <Hider hide={state !== CardStates.EDITTING}>
+          <div className="max-len-controller">
+            <div className="text-set">
+              <p>최대 글자수</p>
+              <input
+                type="text"
+                placeholder={question.maxLen}
+                value={question.maxLen}
+                onChange={maxLenChange}
+              />
+            </div>
+            <div className="btn-set">
+              <button onClick={plus}>
+                <img src={plusBtn} alt="add max length button" />
+              </button>
+              <button onClick={minus}>
+                <img src={minusBtn} alt="minus max length button" />
+              </button>
+            </div>
           </div>
-          <div className="btn-set">
-            <button onClick={plus}>
-              <img src={plusBtn} alt="add max length button" />
-            </button>
-            <button onClick={minus}>
-              <img src={minusBtn} alt="minus max length button" />
-            </button>
-          </div>
-        </div>
+        </Hider>
       </div>
     );
   }
 
   return (
     <div className="short-sentence">
-      <input
-        type="text"
-        placeholder="답변을 입력해주세요."
-        onChange={update}
-        value={question.answer}
-      />
+      <div className="response">
+        <input
+          type="text"
+          placeholder="답변을 입력해주세요."
+          onChange={update}
+          value={question.answer}
+          maxLength={question.maxLen}
+        />
+        <Hider hide={state !== CardStates.EDITTING}>
+          <div className="max-len-indicator">
+            <p className="indicator">현재 / 최대</p>
+            <p>{curLen + " / " + question.maxLen}</p>
+          </div>
+        </Hider>
+      </div>
     </div>
   );
 }
