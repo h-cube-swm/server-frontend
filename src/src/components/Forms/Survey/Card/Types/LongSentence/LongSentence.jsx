862028776760473ac993c853454605a889ed7859
SeungWookHan <hswook12@me.com>
2021-07-22 11:39:16 +0000

    Add LongSentence component and default scss

--- src/src/components/Forms/Survey/Card/Types/LongSentence/LongSentence.jsx
+++ src/src/components/Forms/Survey/Card/Types/LongSentence/LongSentence.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { CardStates } from "../../../constants";
 import addDefault from "../../../../../../utils/addDefault";
+import "./LongSentence.scss";
 
 export default function LongSentence({
   question,
@@ -11,26 +12,28 @@ export default function LongSentence({
 }) {
   if (addDefault(question, setQuestion, { answer: "" })) return null;
 
-  const { answer } = question;
-
-  const handleChange = () => {
-    return;
+  const handleChange = (e) => {
+    const answer = e.target.value;
+    setQuestion((question) => ({ ...question, answer }));
   };
 
-  if (state === CardStates.RESPONSE) {
+  if (state === CardStates.EDITTING) {
     return (
-      <div>
+      <div className="long-sentence">
         <input type="text" placeholder="장문형 텍스트" disabled={true} />
       </div>
     );
   }
   return (
-    <div>
+    <div className="long-sentence">
       <input
         type="text"
         placeholder="답변을 입력하세요"
         onChange={handleChange}
+        value={question.answer}
+        name="response"
       />
+      <p>{question.answer}</p>
     </div>
   );
 }
