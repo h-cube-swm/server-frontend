5bc46d7b336589d945a6b304dee81e5d9004297f
nyeok98 <dn.shin98@gmail.com>
2021-07-22 15:53:54 +0000

    Add style to multiple choice component

--- src/src/components/Forms/CheckField/CheckField.jsx
+++ src/src/components/Forms/CheckField/CheckField.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
 export default function CheckField({
-	checked,
-	setChecked,
-	disabled,
-	...props
+  checked,
+  setChecked,
+  disabled,
+  ...props
 }) {
-	return (
-		<input
-			{...props}
-			type="checkbox"
-			value={checked}
-			onChange={setChecked && ((e) => setChecked(e.target.value))}
-			disabled={disabled || !setChecked}
-		/>
-	);
+  return (
+    <input
+      {...props}
+      type="checkbox"
+      value={checked}
+      onChange={setChecked && ((e) => setChecked(e.target.value))}
+      disabled={disabled || !setChecked}
+    />
+  );
 }
--- src/src/components/Forms/Survey/Card/Types/MultipleChoice/MultipleChoice.jsx
+++ src/src/components/Forms/Survey/Card/Types/MultipleChoice/MultipleChoice.jsx
@@ -6,69 +6,82 @@ import TextField from "../../../../../TextField/TextField";
 import CheckField from "../../../../CheckField/CheckField";
 import { CardStates } from "../../../constants";
 
+import delBtn from "../../../../../../assets/icons/del-btn.svg";
+import addBtn from "../../../../../../assets/icons/add-btn.svg";
+import "./MultipleChoice.scss";
+
 function Choice({ text, setText, checked, setChecked, onDelete, editable }) {
-	return (
-		<div>
-			<CheckField checked={checked} setChecked={setChecked} />
-			<TextField
-				text={text}
-				setText={setText}
-				disabled={!editable}
-				className={editable ? "" : "disabled"}
-			/>
-			<Hider hide={!(editable && onDelete)}>
-				<button onClick={onDelete}>X</button>
-			</Hider>
-		</div>
-	);
+  return (
+    <div className="choice-box">
+      <CheckField
+        className="check-box"
+        checked={checked}
+        setChecked={setChecked}
+      />
+      <TextField
+        text={text}
+        setText={setText}
+        disabled={!editable}
+        className={editable ? "text-box" : "text-box disabled"}
+      />
+      <Hider hide={!(editable && onDelete)}>
+        <button className="del-btn" onClick={onDelete}>
+          <img src={delBtn} alt="delete button" />
+        </button>
+      </Hider>
+    </div>
+  );
 }
 
 export default function MultipleChoice({
-	question,
-	setQuestion,
-	response,
-	setResponse,
-	state,
+  question,
+  setQuestion,
+  response,
+  setResponse,
+  state,
 }) {
-	const initialized = useDefault(question, setQuestion, {
-		choices: ["아이유 이쁘다"],
-	});
-	if (!initialized) return null;
+  const initialized = useDefault(question, setQuestion, {
+    choices: ["아이유 이쁘다"],
+  });
+  if (!initialized) return null;
 
-	if (!question.choices) return null;
+  if (!question.choices) return null;
 
-	const { choices } = question;
-	const editable = state === CardStates.EDITTING;
+  const { choices } = question;
+  const editable = state === CardStates.EDITTING;
 
-	const addChoice = () => {
-		setNestedState(setQuestion, ["choices"])((choices) => [...choices, "인정"]);
-	};
+  const addChoice = () => {
+    setNestedState(setQuestion, ["choices"])((choices) => [...choices, "인정"]);
+  };
 
-	const removeChoice = (i) => {
-		setNestedState(setQuestion, ["choices"])((choices) => {
-			const newChoices = [...choices];
-			newChoices.splice(i, 1);
-			return newChoices;
-		});
-	};
+  const removeChoice = (i) => {
+    setNestedState(setQuestion, ["choices"])((choices) => {
+      const newChoices = [...choices];
+      newChoices.splice(i, 1);
+      return newChoices;
+    });
+  };
 
-	return (
-		<div>
-			{choices.map((choice, i) => {
-				const setText = setNestedState(setQuestion, ["choices", i]);
-				return (
-					<Choice
-						key={i}
-						text={choice}
-						setText={setText}
-						editable={editable}
-						onDelete={() => removeChoice(i)}
-					/>
-				);
-			})}
-			<Hider hide={!editable}>
-				<button onClick={addChoice}>Add New Choice</button>
-			</Hider>
-		</div>
-	);
+  return (
+    <div className="multiple-choice">
+      {choices.map((choice, i) => {
+        const setText = setNestedState(setQuestion, ["choices", i]);
+        return (
+          <Choice
+            key={i}
+            text={choice}
+            setText={setText}
+            editable={editable}
+            onDelete={() => removeChoice(i)}
+          />
+        );
+      })}
+      <Hider hide={!editable}>
+        <button className="add-btn" onClick={addChoice}>
+          <img src={addBtn} alt="delete button" />
+          <p>선택지 추가하기</p>
+        </button>
+      </Hider>
+    </div>
+  );
 }
