2f0d7785ba9697062aa24bf8838c41877cd10b30
nyeok98 <dn.shin98@gmail.com>
2021-07-23 14:42:30 +0000

    Make text field expandable

--- src/src/components/Forms/Survey/Card/Card.jsx
+++ src/src/components/Forms/Survey/Card/Card.jsx
@@ -19,145 +19,147 @@ import TextField from "../../../TextField/TextField";
 import Hider from "../../../Hider/Hider";
 
 function getInnerComponent(type) {
-  switch (type) {
-    case CardTypes.SINGLE_CHOICE:
+	switch (type) {
+		case CardTypes.SINGLE_CHOICE:
 			return MultipleChoice;
-    case CardTypes.MULTIPLE_CHOICE:
-      return MultipleChoice;
-    case CardTypes.PREFERENCE:
-      return Preference;
-    case CardTypes.SHORT_SENTENCE:
-      return ShortSentence;
-    case CardTypes.LONG_SENTENCE:
-      return LongSentence;
-    default:
-      return Default;
-  }
+		case CardTypes.MULTIPLE_CHOICE:
+			return MultipleChoice;
+		case CardTypes.PREFERENCE:
+			return Preference;
+		case CardTypes.SHORT_SENTENCE:
+			return ShortSentence;
+		case CardTypes.LONG_SENTENCE:
+			return LongSentence;
+		default:
+			return Default;
+	}
 }
 
 export default function Card({
-  // Logic-associated parameters
-  state,
-  question,
-  setQuestion,
-  response,
-  setResponse,
-  onDelete,
+	// Logic-associated parameters
+	state,
+	question,
+	setQuestion,
+	response,
+	setResponse,
+	onDelete,
 
-  // UI-associated parameters
-  onGrab,
-  dom,
-  slowAppear,
+	// UI-associated parameters
+	onGrab,
+	dom,
+	slowAppear,
 }) {
-  const isInit = useTimeout(slowAppear ? 400 : 0);
+	const isInit = useTimeout(slowAppear ? 400 : 0);
 
-  if (!question) return null;
+	if (!question) return null;
 
-  let classes = ["survey-card"];
+	let classes = ["survey-card"];
 
-  if (isInit) classes.push("hidden");
+	if (isInit) classes.push("hidden");
 
-  const InnerComponent = getInnerComponent(question.type);
+	const InnerComponent = getInnerComponent(question.type);
 
-  const _onGrab = (event) => {
-    event.preventDefault();
-    if (state !== CardStates.EDITTING) return;
-    onGrab();
-  };
+	const _onGrab = (event) => {
+		event.preventDefault();
+		if (state !== CardStates.EDITTING) return;
+		onGrab();
+	};
 
-  switch (state) {
-    case CardStates.EDITTING:
-      classes.push("highlight");
-      break;
+	switch (state) {
+		case CardStates.EDITTING:
+			classes.push("highlight");
+			break;
 
-    case CardStates.ORDERING:
-      classes.push("hidden");
-      break;
+		case CardStates.ORDERING:
+			classes.push("hidden");
+			break;
 
-    case CardStates.PREVIEW:
-      classes.push("preview");
-      classes.push("hide-handle");
-      break;
+		case CardStates.PREVIEW:
+			classes.push("preview");
+			classes.push("hide-handle");
+			break;
 
-    case CardStates.RESPONSE:
-      classes.push("response");
-      classes.push("hide-handle");
-      break;
+		case CardStates.RESPONSE:
+			classes.push("response");
+			classes.push("hide-handle");
+			break;
 
-    case CardStates.GHOST:
-      classes.push("ghost");
-      classes.push("highlight");
+		case CardStates.GHOST:
+			classes.push("ghost");
+			classes.push("highlight");
 
-      const className = classes.join(" ");
-      return (
-        <div
-          className={className}
-          style={{ height: CardStyle.HEIGHT }}
-          ref={dom}>
-          <div className="card-header">
-            <TextField
-              placeholder="더 폼 나는 질문"
-              text={question.title}
-              size="title"
-            />
-          </div>
-          <div className="inner-box">
-            <InnerComponent
-              state={CardStates.GHOST}
-              isRequired={question.isRequired}
-              question={question}
-            />
-          </div>
-          <div className="handle">
-            <img src={hanleImage} alt="Handle"></img>
-          </div>
-        </div>
-      );
+			const className = classes.join(" ");
+			return (
+				<div
+					className={className}
+					style={{ height: CardStyle.HEIGHT }}
+					ref={dom}>
+					<div className="card-header">
+						<TextField
+							placeholder="더 폼 나는 질문"
+							text={question.title}
+							size="title"
+							multiline
+						/>
+					</div>
+					<div className="inner-box">
+						<InnerComponent
+							state={CardStates.GHOST}
+							isRequired={question.isRequired}
+							question={question}
+						/>
+					</div>
+					<div className="handle">
+						<img src={hanleImage} alt="Handle"></img>
+					</div>
+				</div>
+			);
 
-    default:
-      break;
-  }
+		default:
+			break;
+	}
 
-  const className = classes.join(" ");
-  return (
-    <div className={className} style={{ height: CardStyle.HEIGHT }} ref={dom}>
-      <div className="card-header">
-        <TextField
-          placeholder="더 폼 나는 질문"
-          text={question.title}
-          setText={setNestedState(setQuestion, ["title"])}
-          disabled={state !== CardStates.EDITTING}
-          size="title"
-        />
-        <Hider hide={state !== CardStates.EDITTING}>
-          <div className={`basic-element`}>
-            <ToggleSwitch
-              isRequired={question.isRequired}
-              setIsRequired={setNestedState(setQuestion, ["isRequired"])}
-              label="필수응답"
-            />
-            <button
-              className={"delete" + (onDelete ? "" : " disabled")}
-              onClick={onDelete}>
-              <img src={delBtn} alt="Delete button"></img>
-            </button>
-          </div>
-        </Hider>
-      </div>
-      <div className="inner-box">
-        <InnerComponent
-          state={state}
-          isRequired={question.isRequired}
-          question={question}
-          setQuestion={setQuestion}
-          response={response}
-          setResponse={setResponse}
+	const className = classes.join(" ");
+	return (
+		<div className={className} style={{ height: CardStyle.HEIGHT }} ref={dom}>
+			<div className="card-header">
+				<TextField
+					placeholder="더 폼 나는 질문"
+					text={question.title}
+					setText={setNestedState(setQuestion, ["title"])}
+					disabled={state !== CardStates.EDITTING}
+					size="title"
+					multiline
+				/>
+				<Hider hide={state !== CardStates.EDITTING}>
+					<div className={`basic-element`}>
+						<ToggleSwitch
+							isRequired={question.isRequired}
+							setIsRequired={setNestedState(setQuestion, ["isRequired"])}
+							label="필수응답"
+						/>
+						<button
+							className={"delete" + (onDelete ? "" : " disabled")}
+							onClick={onDelete}>
+							<img src={delBtn} alt="Delete button"></img>
+						</button>
+					</div>
+				</Hider>
+			</div>
+			<div className="inner-box">
+				<InnerComponent
+					state={state}
+					isRequired={question.isRequired}
+					question={question}
+					setQuestion={setQuestion}
+					response={response}
+					setResponse={setResponse}
 					multipleSelect={question.type === CardTypes.MULTIPLE_CHOICE}
-        />
-      </div>
-      <div className="handle" onMouseDown={_onGrab}>
-        <img src={hanleImage} alt="Handle"></img>
-      </div>
-    </div>
-  );
+				/>
+			</div>
+			<div className="handle" onMouseDown={_onGrab}>
+				<img src={hanleImage} alt="Handle"></img>
+			</div>
+		</div>
+	);
 }
--- src/src/components/TextField/TextField.jsx
+++ src/src/components/TextField/TextField.jsx
@@ -6,14 +6,28 @@ function TextField(
 	ref
 ) {
 	if (multiline) {
+		const onInput = ({ target }) => {
+			target.style.height = "";
+			target.style.height = target.scrollHeight + "px";
+			if (size === "title" && target.scrollHeight >= 79) {
+				target.style.height = "79px";
+				target.style.overflow = "scroll";
+			} else if (size === "xl" && target.scrollHeight >= 45) {
+				target.style.height = "45px";
+				target.style.overflow = "scroll";
+			}
+		};
+
 		return (
 			<textarea
+				rows={1}
 				{...props}
 				className={"txt-field textarea " + size}
 				value={text}
 				onChange={setText ? (e) => setText(e.target.value) : () => {}}
 				disabled={disabled || !setText}
 				ref={ref}
+				onInput={onInput}
 			/>
 		);
 	} else {
