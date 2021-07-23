80ee60447d487845c786d279ad0dc90c449240d3
SeungWookHan <hswook12@me.com>
2021-07-23 07:25:18 +0000

    Add TextField with textarea type

--- src/src/components/TextField/TextField.jsx
+++ src/src/components/TextField/TextField.jsx
@@ -1,18 +1,34 @@
 import React, { forwardRef } from "react";
 import "./TextField.scss";
 
-function TextField({ text, setText, disabled, size, ...props }, ref) {
-  return (
-    <input
-      {...props}
-      className={"txt-field " + size}
-      type="text"
-      value={text}
-      onChange={setText ? (e) => setText(e.target.value) : () => {}}
-      disabled={disabled || !setText}
-      ref={ref}
-    />
-  );
+function TextField(
+	{ text, setText, disabled, size, multiline, ...props },
+	ref
+) {
+	if (multiline) {
+		return (
+			<textarea
+				{...props}
+				className={"txt-field textarea " + size}
+				value={text}
+				onChange={setText ? (e) => setText(e.target.value) : () => {}}
+				disabled={disabled || !setText}
+				ref={ref}
+			/>
+		);
+	} else {
+		return (
+			<input
+				{...props}
+				className={"txt-field " + size}
+				type="text"
+				value={text}
+				onChange={setText ? (e) => setText(e.target.value) : () => {}}
+				disabled={disabled || !setText}
+				ref={ref}
+			/>
+		);
+	}
 }
 
 export default forwardRef(TextField);
