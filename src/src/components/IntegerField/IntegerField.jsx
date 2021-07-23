ba03fc1927ffc529206b84e430df2bae31c42cae
nyeok98 <dn.shin98@gmail.com>
2021-07-23 04:28:33 +0000

    Refactor IntegerField with reuse TextField

--- src/src/components/IntegerField/IntegerField.jsx
+++ src/src/components/IntegerField/IntegerField.jsx
@@ -1,45 +1,48 @@
 import React, { forwardRef } from "react";
 import plusBtn from "../../assets/icons/add-btn.svg";
 import minusBtn from "../../assets/icons/minus-btn.svg";
+import TextField from "../TextField/TextField";
 import "./IntegerField.scss";
 
-function IntegerField({ number, setNumber, disabled, ...props }, ref) {
-  const onChange = (e) => {
-    let text = e.target.value + "";
-    if (!/^[0-9]{0,3}$/.test(text)) return false;
-    const number = +text;
-    setNumber(number);
-  };
+function IntegerField({ number, setNumber, disabled, label, ...props }, ref) {
+	const onChange = (text) => {
+		text = text + "";
+		if (!/^[0-9]{0,3}$/.test(text)) return false;
+		const number = +text;
+		setNumber(number);
+	};
 
-  const plus = () => {
-    if (number >= 999) return;
-    setNumber(number + 1);
-  };
+	const plus = () => {
+		if (number >= 999) return;
+		setNumber(number + 1);
+	};
 
-  const minus = () => {
-    if (number <= 0) return;
-    setNumber(number - 1);
-  };
+	const minus = () => {
+		if (number <= 0) return;
+		setNumber(number - 1);
+	};
 
-  return (
-    <span className="integer-field">
-      <input
-        {...props}
-        type="text"
-        value={number}
-        disabled={disabled || !setNumber}
-        onChange={onChange}
-      />
-      <span className="btn-set">
-        <button onClick={plus}>
-          <img src={plusBtn} alt="add max length button" />
-        </button>
-        <button onClick={minus}>
-          <img src={minusBtn} alt="minus max length button" />
-        </button>
-      </span>
-    </span>
-  );
+	return (
+		<span className="integer-field">
+			<div className="text-set">
+				<p className="label">{label}</p>
+				<TextField
+					{...props}
+					text={number}
+					disabled={disabled || !setNumber}
+					setText={onChange}
+				/>
+			</div>
+			<span className="btn-set">
+				<button onClick={plus}>
+					<img src={plusBtn} alt="add max length button" />
+				</button>
+				<button onClick={minus}>
+					<img src={minusBtn} alt="minus max length button" />
+				</button>
+			</span>
+		</span>
+	);
 }
 
 export default forwardRef(IntegerField);
