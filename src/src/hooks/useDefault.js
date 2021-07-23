d584e4ddcd4cd07296b6e43cc358f267ae46cc41
unknownpgr <unknownpgr@gmail.com>
2021-07-23 11:49:53 +0000

    Optimize useDefault

--- src/src/hooks/useDefault.js
+++ src/src/hooks/useDefault.js
@@ -1,26 +1,26 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-export default function useDefault(state, setState, defualts) {
-  const [shouldUpdate, setShouldUpdate] = useState(true);
+export default function useDefault(setState, defaults) {
+  const [initialized, setInitialized] = useState(false);
   useEffect(() => {
+
+    if (initialized) return;
     if (!setState) {
-      console.error("State must be provided when use useDefault hook.");
-      /* 여기 적어도 알러트나 에러가 들어가야 함! */
-      setShouldUpdate(false);
+      setInitialized(true);
       return;
     }
-    let shouldUpdate = false;
-    const newAttributes = {};
-    Object.entries(defualts).forEach(([key, value]) => {
-      if (!(key in state)) {
-        shouldUpdate = true;
-        newAttributes[key] = value;
+
+    setState(state => {
+      if (typeof state === 'object') {
+        return { ...defaults, ...state };
+      } else if (typeof state !== typeof defaults) {
+        return defaults;
+      } else {
+        return state;
       }
     });
-    if (shouldUpdate) {
-      setState(state => ({ ...state, ...newAttributes }));
-    }
-    setShouldUpdate(shouldUpdate);
-  }, [state, setState]);
-  return !shouldUpdate;
+
+    setInitialized(true);
+  }, [setState, initialized]);
+  return initialized;
 }
