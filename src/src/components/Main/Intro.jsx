a48bbb5c81f77fc7e8472211d6206dadd0830cf8
nyeok98 <dn.shin98@gmail.com>
2021-07-24 05:36:46 +0000

    Fix main page design

--- src/src/components/Main/Intro.jsx
+++ src/src/components/Main/Intro.jsx
@@ -9,19 +9,19 @@ import character2 from "../../assets/images/main-character-2.svg";
 
 function Intro() {
   return (
-    <div className="row-1">
+    <div className="intro">
       <div className="character1">
         <img src={character1} alt="counting man" />
       </div>
       <div className="outer-box">
-        <div className="intro">
+        <div className="message">
           <h1>
             모든 폼을 대신하는
             <br />더 폼나는 설문조사
             <br />
             <strong>더 폼</strong>
           </h1>
-          <Link className="btn-make-survey" to="/forms/survey">
+          <Link className="link-btn-xl" to="/forms/survey">
             설문조사 하러가기
           </Link>
         </div>
