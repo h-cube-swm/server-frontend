d51b2ddf4b7d7c6d751273a9b105aee47f711670
SeungWookHan <hswook12@me.com>
2021-07-28 12:15:16 +0000

    Add put method when survey end

--- src/src/components/Forms/Survey/Edit/Edit.jsx
+++ src/src/components/Forms/Survey/Edit/Edit.jsx
@@ -25,152 +25,155 @@ import Hider from "../../../Hider/Hider";
 import { QuestionProvider } from "../../../../contexts/QuestionContext";
 
 const Edit = ({ surveyId, survey, setSurvey, putSurvey }) => {
-	const [selectedIndex, setSelectedIndex] = useState(0);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
-	const setQuesionType = setNestedState(setSurvey, [
-		"questions",
-		selectedIndex,
-		"type",
-	]);
+  const setQuesionType = setNestedState(setSurvey, [
+    "questions",
+    selectedIndex,
+    "type",
+  ]);
 
-	const insertQuestion = (index) => {
-		setSurvey((survey) => {
-			const [counter, question] = getQuestion(survey.counter);
-			const questions = [...survey.questions];
-			questions.splice(index, 0, question);
-			return { ...survey, counter, questions };
-		});
-		setSelectedIndex(index);
-	};
+  const insertQuestion = (index) => {
+    setSurvey((survey) => {
+      const [counter, question] = getQuestion(survey.counter);
+      const questions = [...survey.questions];
+      questions.splice(index, 0, question);
+      return { ...survey, counter, questions };
+    });
+    setSelectedIndex(index);
+  };
 
-	const removeQuestion = (index) => {
-		if (survey.questions.length <= 1) return;
-		setSurvey((survey) => {
-			const questions = [...survey.questions];
-			questions.splice(index, 1);
-			return { ...survey, questions };
-		});
-		if (index === survey.questions.length - 1) {
-			setSelectedIndex(index - 1);
-		}
-	};
+  const removeQuestion = (index) => {
+    if (survey.questions.length <= 1) return;
+    setSurvey((survey) => {
+      const questions = [...survey.questions];
+      questions.splice(index, 1);
+      return { ...survey, questions };
+    });
+    if (index === survey.questions.length - 1) {
+      setSelectedIndex(index - 1);
+    }
+  };
 
-	const [onWheel, isMoving] = useScrollPaging((delta) => {
-		setSelectedIndex((index) => {
-			let newIndex = index + delta;
-			if (newIndex < 0) return index;
-			if (newIndex >= survey.questions.length) return index;
-			return newIndex;
-		});
-	});
+  const [onWheel, isMoving] = useScrollPaging((delta) => {
+    setSelectedIndex((index) => {
+      let newIndex = index + delta;
+      if (newIndex < 0) return index;
+      if (newIndex >= survey.questions.length) return index;
+      return newIndex;
+    });
+  });
 
-	const [onGrab, backgroundCallbacks, item, isDragging] = useDragPaging(
-		(delta) => {
-			let newIndex = selectedIndex + delta;
-			if (newIndex < 0) return;
-			if (newIndex >= survey.questions.length) return;
-			if (newIndex === selectedIndex) return;
+  const [onGrab, backgroundCallbacks, item, isDragging] = useDragPaging(
+    (delta) => {
+      let newIndex = selectedIndex + delta;
+      if (newIndex < 0) return;
+      if (newIndex >= survey.questions.length) return;
+      if (newIndex === selectedIndex) return;
 
-			const questions = [...survey.questions];
-			const tmp = questions[selectedIndex];
-			questions[selectedIndex] = questions[newIndex];
-			questions[newIndex] = tmp;
+      const questions = [...survey.questions];
+      const tmp = questions[selectedIndex];
+      questions[selectedIndex] = questions[newIndex];
+      questions[newIndex] = tmp;
 
-			setSelectedIndex(newIndex);
-			setSurvey({ ...survey, questions });
-		}
-	);
+      setSelectedIndex(newIndex);
+      setSurvey({ ...survey, questions });
+    }
+  );
 
-	let contents = null;
-	if (survey) {
-		const selectedSurveyType = survey.questions[selectedIndex]?.type;
-		const showAddButton = !isDragging && !isMoving;
-		const { questions } = survey;
+  let contents = null;
+  if (survey) {
+    const selectedSurveyType = survey.questions[selectedIndex]?.type;
+    const showAddButton = !isDragging && !isMoving;
+    const { questions } = survey;
 
-		contents = (
-			<>
-				<div ref={item}>
-					<QuestionProvider
-						state={CardStates.GHOST}
-						question={questions[selectedIndex]}>
-						<Card slowAppear={false} />
-					</QuestionProvider>
-				</div>
+    contents = (
+      <>
+        <div ref={item}>
+          <QuestionProvider
+            state={CardStates.GHOST}
+            question={questions[selectedIndex]}>
+            <Card slowAppear={false} />
+          </QuestionProvider>
+        </div>
 
-				<div className="positioning-box">
-					<div className="controller-box">
-						<Controller type={selectedSurveyType} setType={setQuesionType} />
-						<Link className="link-btn" to={"/forms/survey/end/" + surveyId}>
-							완료
-						</Link>
-						<button onClick={putSurvey}>저장</button>
-					</div>
-					<div className="sidebar-box">
-						<Sidebar
-							questions={questions}
-							currentIndex={selectedIndex}
-							onSelect={setSelectedIndex}
-						/>
-					</div>
-				</div>
-				<Prologue survey={survey} setSurvey={setSurvey} />
+        <div className="positioning-box">
+          <div className="controller-box">
+            <Controller type={selectedSurveyType} setType={setQuesionType} />
+            <Link
+              onClick={putSurvey}
+              className="link-btn"
+              to={"/forms/survey/end/" + surveyId}>
+              완료
+            </Link>
+            <button onClick={putSurvey}>저장</button>
+          </div>
+          <div className="sidebar-box">
+            <Sidebar
+              questions={questions}
+              currentIndex={selectedIndex}
+              onSelect={setSelectedIndex}
+            />
+          </div>
+        </div>
+        <Prologue survey={survey} setSurvey={setSurvey} />
 
-				<div className="question-box" onWheel={onWheel}>
-					{orderedMap(questions, (question, index) => {
-						const isSelected = index === selectedIndex;
+        <div className="question-box" onWheel={onWheel}>
+          {orderedMap(questions, (question, index) => {
+            const isSelected = index === selectedIndex;
 
-						const setQuestion = setNestedState(setSurvey, ["questions", index]);
-						const onDelete =
-							questions.length > 1 ? () => removeQuestion(index) : null;
-						const yPos = (index - selectedIndex) * CardStyle.FRAME_HEIHGT;
-						let slowAppear = questions.length > 1;
-						let state = null;
-						if (isSelected) {
-							state = CardStates.EDITTING;
-						} else {
-							state = CardStates.PREVIEW;
-						}
+            const setQuestion = setNestedState(setSurvey, ["questions", index]);
+            const onDelete =
+              questions.length > 1 ? () => removeQuestion(index) : null;
+            const yPos = (index - selectedIndex) * CardStyle.FRAME_HEIHGT;
+            let slowAppear = questions.length > 1;
+            let state = null;
+            if (isSelected) {
+              state = CardStates.EDITTING;
+            } else {
+              state = CardStates.PREVIEW;
+            }
 
-						return (
-							<Positioner key={question.id} y={yPos}>
-								<Hider hide={isDragging && isSelected} animation={false}>
-									<QuestionProvider
-										state={state}
-										question={question}
-										setQuestion={setQuestion}>
-										<Card
-											onDelete={onDelete}
-											onGrab={onGrab}
-											slowAppear={slowAppear}
-										/>
-									</QuestionProvider>
-								</Hider>
-							</Positioner>
-						);
-					})}
+            return (
+              <Positioner key={question.id} y={yPos}>
+                <Hider hide={isDragging && isSelected} animation={false}>
+                  <QuestionProvider
+                    state={state}
+                    question={question}
+                    setQuestion={setQuestion}>
+                    <Card
+                      onDelete={onDelete}
+                      onGrab={onGrab}
+                      slowAppear={slowAppear}
+                    />
+                  </QuestionProvider>
+                </Hider>
+              </Positioner>
+            );
+          })}
 
-					<QuestionAddButton
-						onClick={() => insertQuestion(selectedIndex)}
-						y={-CardStyle.FRAME_HEIHGT / 2}
-						show={showAddButton}
-					/>
+          <QuestionAddButton
+            onClick={() => insertQuestion(selectedIndex)}
+            y={-CardStyle.FRAME_HEIHGT / 2}
+            show={showAddButton}
+          />
 
-					<QuestionAddButton
-						onClick={() => insertQuestion(selectedIndex + 1)}
-						y={+CardStyle.FRAME_HEIHGT / 2}
-						show={showAddButton}
-					/>
-				</div>
-			</>
-		);
-	} else {
-		<div className="loading-screen">Now Loading...</div>;
-	}
-	return (
-		<div className="edit" {...backgroundCallbacks}>
-			{contents}
-		</div>
-	);
+          <QuestionAddButton
+            onClick={() => insertQuestion(selectedIndex + 1)}
+            y={+CardStyle.FRAME_HEIHGT / 2}
+            show={showAddButton}
+          />
+        </div>
+      </>
+    );
+  } else {
+    <div className="loading-screen">Now Loading...</div>;
+  }
+  return (
+    <div className="edit" {...backgroundCallbacks}>
+      {contents}
+    </div>
+  );
 };
 
 export default withSurvey(Edit);
