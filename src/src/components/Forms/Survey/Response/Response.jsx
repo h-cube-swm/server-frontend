0d853d27f7d539c5799931ffe863d49a94289113
nyeok98 <dn.shin98@gmail.com>
2021-08-02 04:26:57 +0000

    Fix response header ui

--- src/src/components/Forms/Survey/Response/Response.jsx
+++ src/src/components/Forms/Survey/Response/Response.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { QuestionProvider } from "../../../../contexts/QuestionContext";
 import withSurvey from "../../../../hocs/withSurvey";
 import setNestedState from "../../../../utils/setNestedState";
 import Card from "../Card/Card";
+import TextField from "../../../TextField/TextField";
 import { CardStates } from "../constants";
 import "./Response.scss";
+import logo from "../../../../assets/images/logo.png";
 import { postApi } from "../../../../utils/parser";
 
 function Response({ survey, submit, surveyId }) {
-	const [response, setResponse] = useState({});
+  const [response, setResponse] = useState({});
   const [data, setData] = useState({});
   const [error, setError] = useState(false);
 
@@ -25,30 +28,52 @@ function Response({ survey, submit, surveyId }) {
     console.log("error 발생");
   };
 
-	let contents = null;
-	if (survey) {
-		const { questions } = survey;
-		contents = (
-			<div className="question-box">
-				{questions.map((question) => {
-					const { id } = question;
-					return (
-						<QuestionProvider
-							state={CardStates.RESPONSE}
-							question={question}
-							key={id}
-							response={response[id]}
-							setResponse={setNestedState(setResponse, [id])}>
-							<Card slowAppear={false} />
-						</QuestionProvider>
-					);
-				})}
-			</div>
-		);
-	} else {
-		<div className="loading-screen">Now Loading...</div>;
-	}
-	return <div className="response-page">{contents}</div>;
+  let contents = null;
+  if (survey) {
+    const { questions } = survey;
+    contents = (
+      <div className="question-box">
+        {questions.map((question) => {
+          const { id } = question;
+          return (
+            <QuestionProvider
+              state={CardStates.RESPONSE}
+              question={question}
+              key={id}
+              response={response[id]}
+              setResponse={setNestedState(setResponse, [id])}>
+              <Card slowAppear={false} />
+            </QuestionProvider>
+          );
+        })}
+      </div>
+    );
+  } else {
+    <div className="loading-screen">Now Loading...</div>;
+  }
+  return (
+    <div className="response">
+      <div className="survey-header">
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+        </div>
+        <TextField text={survey ? survey.title : ""} size="title" multiline />
+        <TextField
+          text={survey ? survey.description : ""}
+          size="xl"
+          multiline
+        />
+      </div>
+      <div className="contents-box">{contents}</div>
+      <div className="button-box">
+        <button href="#" className="link-btn-lg" onClick={onClick}>
+          완료
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default withSurvey(Response);
