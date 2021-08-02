e67871073a653a85548cfabfa0394e8f4d37cbf9
unknownpgr <unknownpgr@gmail.com>
2021-08-02 05:32:45 +0000

    Refactoring

--- src/src/components/CheckField/CheckField.jsx
+++ src/src/components/CheckField/CheckField.jsx
@@ -2,22 +2,22 @@ import React, { forwardRef } from "react";
 import "./CheckField.scss";
 
 function CheckField({ checked, setChecked, disabled, radio, ...props }, ref) {
-	const classes = ["checkmark"];
+  const classes = ["checkmark"];
 
-	if (radio) classes.push("radio");
-	else classes.push("checkbox");
-	if (!disabled && checked) classes.push("fill");
+  if (radio) classes.push("radio");
+  else classes.push("checkbox");
+  if (!disabled && checked) classes.push("fill");
 
-	return (
-		<div className="check-field">
-			<span
-				ref={ref}
-				class={classes.join(" ")}
-				onClick={() => {
-					if (setChecked && !disabled) setChecked(!checked);
-				}}></span>
-		</div>
-	);
+  return (
+    <div className="check-field">
+      <span
+        ref={ref}
+        className={classes.join(" ")}
+        onClick={() => {
+          if (setChecked && !disabled) setChecked(!checked);
+        }}></span>
+    </div>
+  );
 }
 
 export default forwardRef(CheckField);
