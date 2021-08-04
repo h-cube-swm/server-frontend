b11d998d6e345d72c2afc513d4c4043846443826
unknownpgr <unknownpgr@gmail.com>
2021-08-04 16:09:44 +0000

    Add question - component convert logic

--- src/src/components/Forms/Survey/Result/Result.jsx
+++ src/src/components/Forms/Survey/Result/Result.jsx
@@ -1,13 +1,61 @@
-import React from "react";
-import { Redirect } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
 import { API } from "../../../../utils/apis";
 import Loading from "../../../Loading/Loading";
 import ChoiceView from "./ViewTypes/ChoiceView/ChoiceView";
 import SentenceView from "./ViewTypes/SentenceView/SentenceView";
 import PreferenceView from "./ViewTypes/PreferenceView/PreferenceView";
+import TextField from "../../../TextField/TextField";
+import Tables from "./Tables";
+import { CardTypes } from "../constants";
+
+/* Assets */
 import "./Result.scss";
+import logo from "../../../../assets/images/logo.png";
+
+/**
+ * Refactor survey+answers to
+ * [
+ *   {
+ *      question
+ *      answers
+ *   },
+ *   ...
+ *  ]
+ * @param {Object} survey
+ * @param {Array} answers
+ * @returns Array
+ */
+
+const VIEW_DICT = {
+  [CardTypes.SINGLE_CHOICE]: ChoiceView,
+  [CardTypes.MULTIPLE_CHOICE]: ChoiceView,
+  [CardTypes.SHORT_SENTENCE]: SentenceView,
+  [CardTypes.LONG_SENTENCE]: SentenceView,
+  [CardTypes.PREFERENCE]: PreferenceView,
+};
+
+function reshapeAnswer(survey, answers) {
+  const { questions } = survey;
+  let answerDict = {};
+  const answerList = [];
+
+  questions.forEach((question, i) => {
+    answerDict[question.id] = i;
+    answerList.push({ question, answers: [] });
+  });
+
+  answers.forEach(({ answer }) => {
+    Object.entries(answer).forEach(([key, value]) => {
+      answerList[answerDict[key]].answers.push(value);
+    });
+  });
+
+  return answerList;
+}
 
 export default function Result({ match }) {
+  const [isChart, setIsChart] = useState(true);
   const resultId = match.params.link;
 
   const [result, err] = API.useResponses(resultId);
@@ -18,41 +66,16 @@ export default function Result({ match }) {
 
   const { survey, answers } = result;
   const { questions } = survey;
+  const answerList = reshapeAnswer(survey, answers);
 
-  let answerObj = {};
-  questions.forEach((question) => (answerObj[question.id] = []));
-  answers.forEach(({ answer }) => {
-    console.log(answer);
-    Object.entries(answer).map(([key, value]) => {
-      answerObj[key].push(value);
-    });
-  });
-
-  let contents = [];
-
-  questions.map((question) => {
+  let charts = answerList.map(({ question, answers }) => {
     const type = question.type;
-    switch (type) {
-      case "single-choice":
-      case "multiple-choice":
-        contents.push(
-          <ChoiceView key={question.id} answers={answerObj[question.id]} />
-        );
-        break;
-      case "short-sentence":
-      case "long-sentence":
-        contents.push(
-          <SentenceView key={question.id} answers={answerObj[question.id]} />
-        );
-        break;
-      case "preference":
-        contents.push(
-          <PreferenceView key={question.id} answers={answerObj[question.id]} />
-        );
-        break;
-      default:
-        break;
-    }
+    const key = question.id;
+
+    const View = VIEW_DICT[type];
+    if (!View) return null;
+
+    return <View key={key} question={question} answers={answers}></View>;
   });
 
   return (
