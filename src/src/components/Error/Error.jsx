c09ebd101d57f2a205df249b91901c0c40d178ef
nyeok98 <dn.shin98@gmail.com>
2021-08-04 09:00:24 +0000

    Fix error component composition

--- src/src/components/Error/Error.jsx
+++ src/src/components/Error/Error.jsx
@@ -10,30 +10,42 @@ export default function Error({ match }) {
   let contents = null;
   switch (type) {
     case "published": // published 된 설문을 edit으로 들어갈 때
-      contents = <h1>이미 배포된 설문입니다.</h1>;
+      contents = (
+        <div className="contents">
+          <h1>이미 배포된 설문입니다.</h1>;
+        </div>
+      );
       break;
     case "404": // 404
-      contents = <h1>404</h1>;
+      contents = (
+        <div className="contents">
+          <h1>404</h1>;
+        </div>
+      );
       break;
     case "wrongResultId":
       contents = (
-        <>
+        <div className="contents">
           <h1>설문 결과 링크를 잘못 복사한 것 같습니다.</h1>
           <p>혹시 빠트린 글자가 없는지 확인해보세요.</p>
-        </>
+        </div>
       );
       break;
     case "unexpected":
       contents = (
-        <>
-          <h1>에상하지 못한 에러가 발생하였습니다! </h1>
-          <p>연락 부탁!!</p>
-        </>
+        <div className="contents">
+          <h1>예상치 못한 에러가 발생하였습니다. </h1>
+          <p>메인 페이지의 채널톡을 통해 문의주시면 감사드리겠습니다.</p>
+        </div>
       );
       break;
 
     default:
-      contents = <h1>존재하지 않는 경로입니다.</h1>;
+      contents = (
+        <div className="contents">
+          <h1>존재하지 않는 경로입니다.</h1>
+        </div>
+      );
       break;
   }
 
