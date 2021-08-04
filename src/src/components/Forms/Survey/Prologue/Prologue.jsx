b9ffb0f6f59754f52a836a2a999b97b27619b0d6
nyeok98 <dn.shin98@gmail.com>
2021-08-04 16:14:20 +0000

    Fix header difference

--- src/src/components/Forms/Survey/Prologue/Prologue.jsx
+++ src/src/components/Forms/Survey/Prologue/Prologue.jsx
@@ -14,20 +14,22 @@ export default function Prologue({ survey, setSurvey }) {
             <img src={logo} alt="logo" />
           </Link>
         </div>
-        <TextField
-          text={survey.title}
-          setText={setNestedState(setSurvey, ["title"])}
-          placeholder="더 폼 나는 제목"
-          size="title"
-          multiline
-        />
-        <TextField
-          text={survey.description}
-          setText={setNestedState(setSurvey, ["description"])}
-          placeholder="더 폼 나는 설명"
-          size="xl"
-          multiline
-        />
+        <div className="info">
+          <TextField
+            text={survey.title}
+            setText={setNestedState(setSurvey, ["title"])}
+            placeholder="더 폼 나는 제목"
+            size="title"
+            multiline
+          />
+          <TextField
+            text={survey.description}
+            setText={setNestedState(setSurvey, ["description"])}
+            placeholder="더 폼 나는 설명"
+            size="xl"
+            multiline
+          />
+        </div>
       </div>
     </div>
   );
