f8a29f00815ddce5c602510609e17999645ce8c1
unknownpgr <unknownpgr@gmail.com>
2021-08-05 08:00:15 +0000

    Simplify text field

--- src/src/components/TextField/TextField.jsx
+++ src/src/components/TextField/TextField.jsx
@@ -2,42 +2,38 @@ import React from "react";
 import "./TextField.scss";
 
 function TextField({ text, setText, disabled, size, multiline, ...props }) {
-  if (multiline) {
-    const onInput = ({ target }) => {
-      target.style.height = "";
-      target.style.height = target.scrollHeight + "px";
-      if (size === "title" && target.scrollHeight >= 79) {
-        target.style.height = "79px";
-        target.style.overflow = "scroll";
-      } else if (size === "xl" && target.scrollHeight >= 45) {
-        target.style.height = "45px";
-        target.style.overflow = "scroll";
-      }
-    };
-
-    return (
-      <textarea
-        rows={1}
-        {...props}
-        className={"txt-field textarea " + size}
-        value={text}
-        onChange={setText ? (e) => setText(e.target.value) : () => {}}
-        disabled={disabled || !setText}
-        onInput={onInput}
-      />
-    );
-  } else {
-    return (
-      <input
-        {...props}
-        className={"txt-field " + size}
-        type="text"
-        value={text}
-        onChange={setText ? (e) => setText(e.target.value) : () => {}}
-        disabled={disabled || !setText}
-      />
-    );
+  if (!setText) {
+    return <div className={"txt-field " + size}>{text}</div>;
   }
+
+  const handleOnInput = ({ target }) => {
+    let { value } = target;
+    if (!multiline) value = value.replace(/\n/g, "");
+    target.value = value;
+    setText(value);
+
+    target.style.height = "";
+    target.style.height = target.scrollHeight + "px";
+    if (size === "title" && target.scrollHeight >= 79) {
+      target.style.height = "79px";
+      target.style.overflow = "scroll";
+    } else if (size === "xl" && target.scrollHeight >= 45) {
+      target.style.height = "45px";
+      target.style.overflow = "scroll";
+    }
+  };
+
+  return (
+    <textarea
+      rows={1}
+      {...props}
+      className={"txt-field textarea " + size}
+      value={text}
+      // onChange={handleOnChange}
+      disabled={disabled}
+      onInput={handleOnInput}
+    />
+  );
 }
 
 export default TextField;
