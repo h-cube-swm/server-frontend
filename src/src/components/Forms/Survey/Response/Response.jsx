ef4b8e97735db95463f2b6f142b6470ec66f30df
unknownpgr <unknownpgr@gmail.com>
2021-08-05 19:20:47 +0000

    Implement response page interactions

--- src/src/components/Forms/Survey/Response/Response.jsx
+++ src/src/components/Forms/Survey/Response/Response.jsx
@@ -15,24 +15,69 @@ import TextField from "../../../TextField/TextField";
 import { Link, Redirect } from "react-router-dom";
 import QuestionCommon from "../QuestionCommon/QuestionCommon";
 import Loading from "../../../Loading/Loading";
-import Card from "../Card/Card";
 
-function Response({ survey, surveyId, match }) {
-  const [response, setResponse] = useState({});
-  const [contents, setContents] = useState(<></>);
+function checkEntered(response) {
+  if (response === null) return false;
+  if (typeof response === "undefined") return false;
+  if (typeof response === "object")
+    return Object.values(response).filter((x) => x).length > 0;
+  if (typeof response === "string") return response.length > 0;
 
-  const onSubmit = async () => {
-    const body = { answer: response };
-    await API.postResponse(surveyId, body);
-    setContents(<Redirect to="/forms/survey/response/ending" />);
-  };
+  return true;
+}
+
+function Response({ survey, surveyId }) {
+  const [responses, setResponses] = useState({ index: 0 });
+  const [redirect, setRedirect] = useState(null);
+  const { questions } = survey;
+  const { index } = responses;
+  const question = questions[index];
+  const response = responses[question.id];
+  const setIndex = setNestedState(setResponses, ["index"]);
 
   if (!survey) return <Loading />;
-  const { questions } = survey;
+  if (redirect) return <Redirect to={redirect} />;
+
+  const getMove = (index) => () => setIndex(index);
+  const onSubmit = async () => {
+    const body = { answer: responses };
+    await API.postResponse(surveyId, body);
+    setRedirect("/forms/survey/response/ending");
+  };
+
+  let buttons = [];
+
+  const isAnswered = !question.isRequired || checkEntered(response);
+  if (index > 0) {
+    buttons.push(
+      <button key="previous" className="btn" onClick={getMove(index - 1)}>
+        이전으로
+      </button>
+    );
+  }
+  if (index < questions.length - 1) {
+    buttons.push(
+      <button
+        key="next"
+        className={"btn " + (isAnswered ? "" : "disabled")}
+        onClick={isAnswered ? getMove(index + 1) : () => {}}>
+        다음으로
+      </button>
+    );
+  }
+  if (index === questions.length - 1) {
+    buttons.push(
+      <button
+        key="finished"
+        className={"btn " + (isAnswered ? "" : "disabled")}
+        onClick={isAnswered ? onSubmit : () => {}}>
+        완료
+      </button>
+    );
+  }
 
   return (
     <div className="response">
-      {contents}
       <div className="survey-header">
         <div className="logo">
           <Link to="/">
@@ -49,28 +94,34 @@ function Response({ survey, surveyId, match }) {
         </div>
       </div>
       <div className="contents-box">
-        <div className="question-box">
-          {questions.map((question) => {
-            const { id } = question;
-            return (
-              <QuestionProvider
-                state={CardStates.RESPONSE}
-                question={question}
-                key={id}
-                response={response[id]}
-                setResponse={setNestedState(setResponse, [id])}>
-                <Card>
+        {questions.map((question, i) => {
+          const { id } = question;
+
+          const classes = ["question-box"];
+
+          if (i < index) classes.push("left");
+          if (i > index) classes.push("right");
+
+          const className = classes.join(" ");
+
+          return (
+            <QuestionProvider
+              state={CardStates.RESPONSE}
+              question={question}
+              key={id}
+              response={responses[id]}
+              setResponse={setNestedState(setResponses, [id])}>
+              <div className={className}>
+                <div className="question-box-inner">
                   <QuestionCommon />
-                </Card>
-              </QuestionProvider>
-            );
-          })}
-        </div>
+                </div>
+              </div>
+            </QuestionProvider>
+          );
+        })}
       </div>
       <div className="button-box">
-        <button className="link-btn-lg" onClick={onSubmit}>
-          완료
-        </button>
+        <div className="button-box-inner">{buttons}</div>
       </div>
     </div>
   );
