96d56d1592bc99248f015c9c2cd22a2997aa3413
nyeok98 <dn.shin98@gmail.com>
2021-08-06 18:57:04 +0000

    Redesign ending components

--- src/src/components/Forms/Survey/EditEnding/EditEnding.jsx
+++ src/src/components/Forms/Survey/EditEnding/EditEnding.jsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 import withSurveyEnding from "../../../../hocs/withSurveyEnding";
 
 import "./EditEnding.scss";
+import firework from "../../../../assets/icons/firework.png";
 import logo from "../../../../assets/images/logo.png";
 import Loading from "../../../Loading/Loading";
+import Firework from "../ResponseEnding/Firework/Firework";
 
 const Ending = ({ ending }) => {
   if (!ending) {
@@ -14,34 +16,68 @@ const Ending = ({ ending }) => {
   const { title, description, surveyLink, resultLink } = ending;
 
   return (
-    <div className="ending-page">
+    <div className="edit-ending">
       <div className="logo">
         <Link to="/">
           <img src={logo} alt="logo" />
         </Link>
       </div>
-      <h1 className="celebrate-sentence">
-        축하합니다. 설문을 완성했습니다.
-        <br />
-        이제 배포만 남았습니다.
-      </h1>
-      <div className="survey-sum">
-        <h1>{title}</h1>
-        <h2>{description}</h2>
-        <ul>
-          <li>
-            <h3>배포링크</h3>
-            <p>{"https://the-form.io/forms/survey/response/" + surveyLink}</p>
-          </li>
-          <li>
-            <h3>결과링크</h3>
-            <p>{"https://the-form.io/forms/survey/result/" + resultLink}</p>
-          </li>
-        </ul>
+      <div className="contents-box">
+        <div className="celebrate-box">
+          <div className="celebrate-sentence">
+            <img src={firework} alt="celebrating firework" />
+            <h1>
+              축하합니다. <br />
+              설문을 완성했습니다. <br />
+              이제 배포만 남았습니다!
+            </h1>
+          </div>
+          <Firework />
+          <Link className="btn lg" to="/forms/survey">
+            홈으로
+          </Link>
+        </div>
+        <div className="service-box">
+          <div className="section">
+            <div className="box one">
+              <div className="description">
+                <h1>설문 제목</h1>
+                <h2>{title}</h2>
+              </div>
+            </div>
+            <div className="box four">
+              <div className="description">
+                <h1>설문 설명</h1>
+                <h3>{description}</h3>
+              </div>
+            </div>
+            <div className="box five">
+              <div className="description">
+                <h1>
+                  아래 링크를 통해
+                  <br />
+                  사람들에게 <strong>배포</strong>하세요.
+                </h1>
+                <h3>
+                  {"https://the-form.io/forms/survey/response/" + surveyLink}
+                </h3>
+              </div>
+            </div>
+            <div className="box six">
+              <div className="description">
+                <h1>
+                  아래 링크를 통해
+                  <br />
+                  <strong>결과</strong>를 확인하세요.
+                </h1>
+                <h3>
+                  {"https://the-form.io/forms/survey/result/" + resultLink}
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <Link className="btn lg" to="/">
-        홈으로
-      </Link>
     </div>
   );
 };
--- src/src/components/Forms/Survey/ResponseEnding/ResponseEnding.jsx
+++ src/src/components/Forms/Survey/ResponseEnding/ResponseEnding.jsx
@@ -7,6 +7,9 @@ import "./ResponseEnding.scss";
 import logo from "../../../../assets/images/logo.png";
 import firework from "../../../../assets/icons/firework.png";
 import Firework from "./Firework/Firework";
+import pencil from "../../../../assets/images/pencil.png";
+import books from "../../../../assets/images/books.png";
+import wand from "../../../../assets/images/wand.png";
 
 export default function ResponseEnding() {
   return (
@@ -16,18 +19,51 @@ export default function ResponseEnding() {
           <img src={logo} alt="logo" />
         </Link>
       </div>
-      <div className="celebrate-sentence">
-        <img src={firework} alt="celebrating firework" />
-        <h1>
-          축하합니다. <br />
-          설문이 제출되었습니다.
-        </h1>
-        <img className="right" src={firework} alt="celebrating firework" />
+      <div className="contents-box">
+        <div className="celebrate-box">
+          <div className="celebrate-sentence">
+            <img src={firework} alt="celebrating firework" />
+            <h1>
+              축하합니다. <br />
+              설문이 제출되었습니다.
+            </h1>
+          </div>
+          <Firework />
+          <Link className="btn lg" to="/forms/survey">
+            설문조사 <br />
+            시작하기
+          </Link>
+        </div>
+        <div className="service-box">
+          <div className="section">
+            <div className="box one">
+              <div className="description">
+                <h1>편하게,</h1>
+                <h1>쉽게,</h1>
+                <h1>빠르게,</h1>
+                <h1>응답하고</h1>
+              </div>
+              <img src={pencil} alt="pencil" />
+            </div>
+            <div className="box four">
+              <img src={wand} alt="wand" />
+              <div className="description">
+                <h1>간단하게,</h1>
+                <h1>직관적으로,</h1>
+                <h1>멋지게,</h1>
+                <h1>만들고</h1>
+              </div>
+            </div>
+            <div className="box five">
+              <div className="description">
+                <h1>더 폼 나게</h1>
+                <h1>결과를 확인합니다.</h1>
+              </div>
+              <img src={books} alt="books" />
+            </div>
+          </div>
+        </div>
       </div>
-      <Firework />
-      <Link className="btn xl" to="/forms/survey">
-        설문조사 하러가기
-      </Link>
     </div>
   );
 }
