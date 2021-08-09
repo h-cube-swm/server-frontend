0bf0a3d9d5ebb65d2bef2e6bcbde676d4930dfb7
unknownpgr <unknownpgr@gmail.com>
2021-08-09 19:31:35 +0000

    Remove callback caching

--- src/src/contexts/QuestionContext.js
+++ src/src/contexts/QuestionContext.js
@@ -3,20 +3,7 @@ import { createContext, useContext, useRef } from "react";
 const QuestionContext = createContext();
 
 export function QuestionProvider({ children, state, question, setQuestion, response, setResponse }) {
-  const callbackRef = useRef({});
-  const callbacks = callbackRef.current;
-
-  if (callbacks.question !== question || callbacks.question === undefined) {
-    callbacks.question = question;
-    callbacks['setQuestion'] = setQuestion;
-  }
-
-  if (callbacks.response !== response || callbacks.response === undefined) {
-    callbacks.response = response;
-    callbacks['setResponse'] = setResponse;
-  }
-
-  return <QuestionContext.Provider value={{ state, question, response, ...callbacks }} >
+  return <QuestionContext.Provider value={{ state, question, response, setQuestion, setResponse }} >
     {children}
   </QuestionContext.Provider >;
 }
