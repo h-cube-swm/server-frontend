5010400fa894f030c26a4511fd54283177263a58
unknownpgr <unknownpgr@gmail.com>
2021-08-09 19:32:15 +0000

    Update prologue to be foldable

--- src/src/components/Forms/Survey/Prologue/Prologue.jsx
+++ src/src/components/Forms/Survey/Prologue/Prologue.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import setNestedState from "../../../../utils/setNestedState";
 import TextField from "../../../TextField/TextField";
 import "./Prologue.scss";
 import logo from "../../../../assets/images/logo.png";
+import arrow from "../../../../assets/icons/upward-arrow.svg";
 
-export default function Prologue({ survey, setSurvey, putSurvey }) {
+export default function Prologue({ survey, setSurvey, putSurvey, setIsEnded }) {
+  const [isFolded, setIsFolded] = useState(true);
+  const classes = ["prologue-box"];
+  if (isFolded) classes.push("folded");
+  else classes.push("extended");
+
+  const onClick = () => {
+    setIsEnded(true);
+    return putSurvey;
+  };
+
+  const className = classes.join(" ");
   return (
-    <div className="prologue-box">
+    <div className={className}>
       <div className="survey-header">
         <div className="logo">
           <Link to="/">
@@ -15,31 +27,34 @@ export default function Prologue({ survey, setSurvey, putSurvey }) {
           </Link>
         </div>
         <div className="info">
+          <div className="title-box">
+            <TextField
+              text={survey.title}
+              setText={setNestedState(setSurvey, ["title"])}
+              placeholder="더 폼 나는 제목"
+              size="title"
+              multiline
+              onFocus={() => setIsFolded(false)}
+            />
+          </div>
           <TextField
-            text={survey.title}
-            setText={setNestedState(setSurvey, ["title"])}
-            placeholder="더 폼 나는 제목"
-            size="title"
-            multiline
-          />
-          <TextField
+            className="description-box"
             text={survey.description}
             setText={setNestedState(setSurvey, ["description"])}
             placeholder="더 폼 나는 설명"
             size="xl"
             multiline
+            onFocus={() => setIsFolded(false)}
           />
         </div>
-        <div className="put-survey">
-          {putSurvey ? (
-            <Link
-              onClick={putSurvey}
-              className="btn submit-button"
-              to={"/forms/survey/end/" + survey.id}>
-              완료
-            </Link>
-          ) : null}
-        </div>
+        <button onClick={onClick} className="btn rg submit-button">
+          완료
+        </button>
+      </div>
+      <div className="fold-button-box">
+        <button onClick={() => setIsFolded(true)}>
+          <img src={arrow} alt="arrow to fold" />
+        </button>
       </div>
     </div>
   );
