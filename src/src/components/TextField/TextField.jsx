0789b0365c9f55c47271f97c8b2eb26bb83f257b
unknownpgr <unknownpgr@gmail.com>
2021-08-09 19:33:19 +0000

    Fix textfield

--- src/src/components/TextField/TextField.jsx
+++ src/src/components/TextField/TextField.jsx
@@ -1,16 +1,6 @@
 import React, { useEffect, useRef } from "react";
+import useScrollBlock from "../../hooks/useScrollBlock";
 import "./TextField.scss";
-
-/*
-
-  if disabled : font color is gray instead of black, and not editable.
-  else        : font color is black.
-
-  if setText is supplied : editable
-  else                   : not editable
-
-  if both setText is supplied and disabled : not editable.
- */
 
 function TextField({
   text,
@@ -19,39 +9,27 @@ function TextField({
   size,
   multiline,
   placeholder,
+  focused,
   ...props
 }) {
   const ref = useRef(null);
 
   // Build classname
-  const classes = ["txt-field"];
+  const classes = ["text-field"];
   if (size) classes.push(size);
-  if (disabled) classes.push("disabled");
-  if (!text) classes.push("placeholder");
+  if (disabled) classes.push("gray viewmode");
+  if (!text) classes.push("gray");
+  if (!setText) classes.push("viewmode");
   if (multiline) classes.push("multiline");
   const className = classes.join(" ");
 
-  // Adjust maximum height. It works similar to max-height option of css.
-  const resize = () => {
-    const cur = ref.current;
-
-    if (!cur) return;
-    cur.style.height = "";
-    const height = Math.min(cur.scrollHeight + 2, 100) + "px";
-    cur.style.height = height;
-  };
-
   const handleOnInput = () => {
     const cur = ref.current;
-
     if (disabled) return;
     if (!cur) return;
-    let { value } = cur;
-    if (!multiline) value = value.replace(/\n/g, "");
-    setText(value);
+    if (!multiline) cur.value = cur.value.replace(/\n/g, "");
+    setText(cur.value);
   };
-
-  resize();
 
   if (!setText) {
     if (text) {
@@ -63,13 +41,13 @@ function TextField({
 
   return (
     <textarea
+      className={className}
       ref={ref}
       rows={1}
       {...props}
-      className={className}
       onInput={handleOnInput}
       value={text}
-      disabled={disabled}
+      disabled={disabled || !setText}
       placeholder={placeholder}
     />
   );
