7491b0b0d7b7f6174aa51fc28fccdddaad05412f
unknownpgr <unknownpgr@gmail.com>
2021-08-10 08:19:14 +0000

    Add choice filtering

--- src/src/hocs/withSurvey.js
+++ src/src/hocs/withSurvey.js
@@ -1,4 +1,5 @@
 import { Redirect } from "react-router-dom";
+import { CardTypes } from "../components/Forms/Survey/constants";
 import getQuestion from "../components/Forms/Survey/getQuestion";
 import Loading from "../components/Loading/Loading";
 import { API } from "../utils/apis";
@@ -7,10 +8,12 @@ const withSurvey = Component => props => {
 
   const surveyId = props.match.params.link;
 
+  // Load survey data from server
   const [result, err] = API.useSurvey(surveyId);
   if (err) return <Redirect to="/error/unexpected/cannot-load-survey"></Redirect>;
   if (!result) return <Loading></Loading>;
 
+  // Reshape survey structure into one object
   const { title, description, contents, ...meta } = result;       //A
   let survey = { title, description, meta, ...contents };         //B
   survey.id = surveyId;
@@ -25,8 +28,23 @@ const withSurvey = Component => props => {
   const newProps = { ...props, surveyId, survey };
 
   async function updateSurvey(survey) {
+    // Copy survey
+    survey = JSON.parse(JSON.stringify(survey));
+
+    // Filter empty choices of single choice or multiple choice
+    survey.questions = survey.questions.map((question) => {
+      const { type } = question;
+      if (type == CardTypes.SINGLE_CHOICE || type == CardTypes.MULTIPLE_CHOICE) {
+        question.choices = question.choices.filter(x => x);
+      }
+      return question;
+    });
+
+    // Reshape survey object
     const { title, description, meta, ...contents } = survey;     //B
     const body = { title, description, contents, view: "slide" }; //A
+
+    // Send data to server
     return await API.putSurvey(surveyId, body);
   }
 
