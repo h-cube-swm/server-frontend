af9d76091faade278036eeb214e900419c81ba6b
unknownpgr <unknownpgr@gmail.com>
2021-08-12 07:22:23 +0000

    Update hider not to render hidden component

--- src/src/components/Hider/Hider.jsx
+++ src/src/components/Hider/Hider.jsx
@@ -6,13 +6,9 @@ function Hider({ hide, children, animation = true, appearDelay = 0 }) {
   const isInit = useTimeout(appearDelay);
   hide |= !isInit;
 
-  return (
-    <div
-      className={`hider ${hide ? "hide" : ""}`}
-      style={{ transitionDuration: animation ? "500ms" : "0s" }}>
-      {children}
-    </div>
-  );
+  if (hide) return null;
+
+  return children;
 }
 
 export default Hider;
