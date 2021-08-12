83180e78f2c7abbda3e6737ec7d9826c83ae5124
SeungWookHan <hswook12@me.com>
2021-08-12 07:03:37 +0000

    Initialize GetWinner Component

--- src/src/components/Forms/Survey/Result/GetWinner/GetWinner.jsx
+++ src/src/components/Forms/Survey/Result/GetWinner/GetWinner.jsx
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function GetWinner() {
-  return <div></div>;
+export default function GetWinner({ changeOpen, answers }) {
+  const SELECT_DICT = {};
+  const [isRandom, setIsRandom] = useState(false);
+  const [isOrder, setIsOrder] = useState(false);
+
+  const changeRandom = () => {
+    setIsRandom(!isRandom);
+  };
+  const changeOrder = () => {
+    setIsOrder(!isOrder);
+  };
+  return (
+    <div className="get-winner">
+      <button onClick={changeOpen}>뒤로가기</button>
+      <button onClick={changeRandom}>랜덤 추첨</button>
+      <button onClick={changeOrder}>시간순 추첨</button>
+      <input type="text" />
+      <div>{isRandom && <h3>랜덤 추출</h3>}</div>
+    </div>
+  );
 }
