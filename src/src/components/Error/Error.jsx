6e8c49337e2ef5df727e92ee07c30541995445c4
unknownpgr <unknownpgr@gmail.com>
2021-08-12 18:46:51 +0000

    Remove unused dependencies

--- src/src/components/Error/Error.jsx
+++ src/src/components/Error/Error.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
-import Chaos from "../Chaos/Chaos";
 import "./Error.scss";
 
 export default function Error({ match }) {
