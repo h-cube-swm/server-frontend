0d0d9daa1e94792b2e9b6c2bca3268e26f9d52b3
SeungWookHan <hswook12@me.com>
2021-08-13 05:55:28 +0000

    Add max, min props for check number

--- src/src/components/IntegerField/IntegerField.jsx
+++ src/src/components/IntegerField/IntegerField.jsx
@@ -4,21 +4,28 @@ import minusBtn from "../../assets/icons/minus-btn.svg";
 import TextField from "../TextField/TextField";
 import "./IntegerField.scss";
 
-function IntegerField({ number, setNumber, disabled, label, ...props }, ref) {
+function IntegerField(
+  { number, setNumber, disabled, label, max, min, ...props },
+  ref
+) {
+  const maxNum = max ? max : 999;
+  const minNum = min ? min : 0;
+
   const onChange = (text) => {
     text = text + "";
     if (!/^[0-9]{0,3}$/.test(text)) return false;
-    const number = +text;
+    let number = +text;
+    if (number >= maxNum) number = maxNum;
     setNumber(number);
   };
 
   const plus = () => {
-    if (number >= 999) return;
+    if (number >= maxNum) return;
     setNumber(number + 1);
   };
 
   const minus = () => {
-    if (number <= 0) return;
+    if (number <= minNum) return;
     setNumber(number - 1);
   };
 
@@ -32,6 +39,8 @@ function IntegerField({ number, setNumber, disabled, label, ...props }, ref) {
           disabled={disabled || !setNumber}
           setText={onChange}
           size="md"
+          max={maxNum}
+          min={minNum}
         />
       </div>
       <span className="btn-set">
