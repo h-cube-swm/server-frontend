f5f361b1c27725c156becd84fef35aab6c739874
unknownpgr <unknownpgr@gmail.com>
2021-08-13 03:27:38 +0000

    Fix floating logo error

--- src/src/components/FloatingLogo/FloatingLogo.jsx
+++ src/src/components/FloatingLogo/FloatingLogo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
+import "./FloatingLogo.scss";
 
 export default function FloatingLogo() {
   return (
