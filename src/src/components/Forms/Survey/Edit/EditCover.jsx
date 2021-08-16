4c8a2e3c0f9234de9414de6fe8bef1e5a3b4ae18
nyeok98 <dn.shin98@gmail.com>
2021-08-16 05:55:07 +0000

    Fix typo error

--- src/src/components/Forms/Survey/Edit/EditCover.jsx
+++ src/src/components/Forms/Survey/Edit/EditCover.jsx
@@ -9,7 +9,7 @@ export default function EditCover() {
         <h1>
           모바일 환경에서 <br />폼 제작을 권장하지 않습니다.
         </h1>
-        <p>PC, 태블릿pc 등을 이용해주세요.</p>
+        <p>PC, 태블릿PC 등을 이용해주세요.</p>
       </div>
       <Link className="btn rg home-btn" to="/">
         홈으로
