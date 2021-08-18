e9cb0ec7af6c44f726512da7e777bdbc393701a3
nyeok98 <dn.shin98@gmail.com>
2021-08-18 09:00:14 +0000

    Add beta tag in main page

--- src/src/components/Main/Intro.jsx
+++ src/src/components/Main/Intro.jsx
@@ -1,12 +1,18 @@
 /* React elements */
 import { React } from "react";
 import { Link } from "react-router-dom";
+import EditCover from "../Forms/Survey/Edit/EditCover";
 
 /* Styles */
 import "./Intro.scss";
 import Chaos from "../Chaos/Chaos";
 
 function Intro() {
+  let isMobile = false;
+  if (document.getElementById("root").offsetWidth < 767) {
+    isMobile = true;
+  }
+
   return (
     <div className="intro">
       <div className="fade-out" />
@@ -17,9 +23,13 @@ function Intro() {
             모든 폼을 대신하는
             <br />더 폼나는 설문조사
             <br />
-            <strong>더 폼</strong>
+            <strong>
+              더 폼<p className="beta-tag">beta</p>
+            </strong>
           </h1>
-          <Link className="btn long" to="/forms/survey">
+          <Link
+            className="btn long"
+            to={isMobile ? "/forms/survey/mobile" : "/forms/survey"}>
             설문조사 만들기
           </Link>
         </div>
