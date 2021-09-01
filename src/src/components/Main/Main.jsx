dcf7fb4032d58cb9d7c5d0898e9241832e66da00
dongnyeok <dn.shin98@gmail.com>
2021-09-01 13:28:38 +0000

    Add a tag in contact email

--- src/src/components/Main/Main.jsx
+++ src/src/components/Main/Main.jsx
@@ -19,7 +19,9 @@ const Main = () => {
           <div className="description">
             <h1 className="company">더 폼</h1>
             <h4>주소: 서울특별시 강남구 테헤란로 311(역삼동) 아남타워빌딩 7층(06151)</h4>
-            <h4>제휴·협력문의: support@the-from.io</h4>
+            <h4>
+              제휴·협력문의: <a href="mailto:contact@the-form.io">contact@the-form.io</a>
+            </h4>
           </div>
           <p className="copyright">Copyright 2021 H-cube Co. all rights reserved.</p>
         </div>
