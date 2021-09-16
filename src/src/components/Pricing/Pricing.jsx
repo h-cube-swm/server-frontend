38baa6c621a6d21565bee22ff00b5983d043961d
dongnyeok <dn.shin98@gmail.com>
2021-09-16 10:33:34 +0000

    Fix typo error

--- src/src/components/Pricing/Pricing.jsx
+++ src/src/components/Pricing/Pricing.jsx
@@ -25,7 +25,7 @@ export default function Pricing() {
           <li>6가지 질문 세트</li>
           <li>흐름 설정 기능</li>
           <li>링크를 통한 간편한 설문 배포, 결과 확인</li>
-          <li>.xslx .png 등의 결과추출</li>
+          <li>.xlsx .png 등의 결과추출</li>
           <li>결과차트 제공</li>
           <li>임베드 코드 제공</li>
           <li>질문 자동추천</li>
@@ -38,7 +38,8 @@ export default function Pricing() {
         <p className="free">이 모든 것이 무료</p>
         <span className="comment">
           <p>
-            * 기업 및 단체 고객의 경우 문의를 통한 자사 서비스 이용 시 홍보에 사용될 수 있습니다.
+            * 기업 및 단체 고객의 경우 자사 서비스 이용 시 기업 혹은 단체명이 홍보에 사용될 수
+            있습니다.
           </p>
           <p>* 무료 사용자의 경우 제작한 설문이 질문 추천 기능의 성능 개선에 사용될 수 있습니다.</p>
         </span>
