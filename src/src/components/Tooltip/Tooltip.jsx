8a7b9ea79288a61a7774c0915018fb122e03fd21
dongnyeok <dn.shin98@gmail.com>
2021-09-24 17:30:34 +0000

    Add position in tooltip

--- src/src/components/Tooltip/Tooltip.jsx
+++ src/src/components/Tooltip/Tooltip.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Tooltip.scss";
 
-export default function Tooltip({ children, text, size }) {
+export default function Tooltip({ children, text, size, pos }) {
+  const classes = ["tooltiptext"];
+  if (size) classes.push(size);
+  if (pos) classes.push(pos);
   return (
     <div className="tooltip">
       {children}
-      <span className={"tooltiptext " + size}>{text}</span>
+      <span className={classes.join(" ")}>{text}</span>
     </div>
   );
 }
