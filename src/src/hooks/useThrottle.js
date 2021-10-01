fa06a5d0289e8747998c870762f8f2ae78197be0
SeungWookHan <hswook12@me.com>
2021-10-01 07:12:17 +0000

    Add useThrottleWithTimeout function

--- src/src/hooks/useThrottle.js
+++ src/src/hooks/useThrottle.js
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function useThrottle(callback) {
   const timeoutHandle = useRef(0);
@@ -9,6 +9,34 @@ export default function useThrottle(callback) {
         callback();
       }
     }, 1000);
+
+    timeoutHandle.current = handle;
+  }
+
+  return onEvent;
+}
+
+export function useThrottleWithTimeout(callback, timeout = 1000) {
+  const timeoutHandle = useRef(0);
+  const [isFirst, setIsFirst] = useState(true);
+
+  function onEvent() {
+    const previous = timeoutHandle.current;
+
+    if (previous) {
+      return;
+    }
+    if (isFirst) {
+      callback();
+      setIsFirst(false);
+    }
+
+    const handle = setTimeout(() => {
+      if (timeoutHandle.current === handle) {
+        callback();
+      }
+      timeoutHandle.current = 0;
+    }, timeout);
 
     timeoutHandle.current = handle;
   }
