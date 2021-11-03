30c651757cafc9f93907ccdf715525521a756b35
unknownpgr <unknownpgr@gmail.com>
2021-11-03 03:04:34 +0000

    Update header design

--- src/src/components/Main/Header.jsx
+++ src/src/components/Main/Header.jsx
@@ -3,13 +3,13 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useGlobalState } from "../../contexts/GlobalContext";
 import { DOMAIN } from "../../constants";
-import FloatingLogo from "../FloatingLogo/FloatingLogo";
 import "./Header.scss";
+import logo from "../../assets/images/logo.png";
 
-function Header() {
+function Header({ isScolled }) {
   const { token, logout } = useGlobalState();
   const location = `https://${DOMAIN}${useLocation().pathname}`;
-  const href = `https://auth.the-form.io?redirect=${location}`;
+  const loginHref = `https://auth.the-form.io?redirect=${location}`;
 
   function handleLogout() {
     logout();
@@ -17,8 +17,12 @@ function Header() {
 
   return (
     <div className="header">
-      <div className="header-box">
-        <FloatingLogo />
+      <div className={isScolled ? "header-box scrolled" : "header-box"}>
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+        </div>
         <div className="banner">
           <Link to="/pricing">가격안내</Link>
           {token ? (
@@ -29,7 +33,7 @@ function Header() {
               </a>
             </>
           ) : (
-            <a className="login" href={href}>
+            <a className="login" href={loginHref}>
               로그인
             </a>
           )}
