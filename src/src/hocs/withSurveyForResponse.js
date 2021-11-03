2d6807fd4ed4041c45ba76d7a67e7cc48529a00a
dongnyeok <dn.shin98@gmail.com>
2021-11-03 18:02:24 +0000

    Specify error page

--- src/src/hocs/withSurveyForResponse.js
+++ src/src/hocs/withSurveyForResponse.js
@@ -3,6 +3,7 @@ import Loading from "../components/Loading/Loading";
 import API from "../utils/apis";
 import { tryUnhash } from "../utils/hasher";
 import Error from "../components/Error/Error";
+import { SurveyStatus } from "../constants";
 
 const withSurvey = (Component) => (props) => {
   let surveyId = props.match.params.link;
@@ -12,6 +13,8 @@ const withSurvey = (Component) => (props) => {
   const [survey, err] = API.useSurvey(surveyId, "response");
   if (err) return <Error type="cannot-load-survey-for-response" />;
   if (!survey) return <Loading />;
+  if (survey.status === SurveyStatus.FINISHED) return <Error type="finished-survey" />;
+  if (survey.status === SurveyStatus.EDITING) return <Error type="editing-survey" />;
   const newProps = { ...props, survey };
   return <Component {...newProps} />;
 };
