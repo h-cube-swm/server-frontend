0bcb9d71dfdf18cd238dbe5362e9e997cd048e39
unknownpgr <unknownpgr@gmail.com>
2021-11-03 15:15:35 +0000

    Fix docker.sh

--- docker.sh
+++ docker.sh
@@ -3,8 +3,7 @@
 docker run          \
 --rm                \
 -it                 \
--v $(pwd):/app      \
+-v $(pwd)/src:/app  \
 -w /app             \
--p 3000:3000        \
 node:14             \
-/bin/bash -c "yarn; (echo Start compile. Development server will be opened at port 3000.;yarn start 1>/dev/null 2>/dev/null) & /bin/bash"
+/bin/bash
